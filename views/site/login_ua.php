632d640326f90b08566f159f7fd617a7ed81c515
Oleksii <alekseyyp@gmail.com>
2016-03-29 11:06:25 +0000

    Implement home page graphics + contact + login

--- views/site/login_ua.php
+++ views/site/login_ua.php
@@ -39,8 +39,8 @@ $this->params['breadcrumbs'][] = $this->title;
         'id' => 'login-form',
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-8 col-md-8\">{input}</div>\n<div class=\"col-lg-9 col-lg-offset-3 col-md-9 col-md-offset-3 col-sm-9 col-sm-offset-3 col-xs-offset-4\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-3 col-md-3 col-sm-3 control-label'],
+            'template' => "{label}\n<div class=\"col-lg-8 col-md-8 col-sm-10 col-xs-9\">{input}</div>\n<div class=\"col-lg-9 col-lg-offset-3 col-md-9 col-md-offset-3 col-sm-9 col-sm-offset-2 col-xs-offset-3 \">{error}</div>",
+            'labelOptions' => ['class' => 'col-lg-3 col-md-3 col-sm-2 col-xs-3 control-label'],
         ],
     ]); ?>
 
@@ -51,7 +51,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'password')->passwordInput() ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-3 col-lg-3 col-md-offset-3 col-md-4 col-sm-offset-2 col-sm-6 \">{input} {label}</div>\n
+            'template' => "<div class=\"col-lg-offset-3 col-lg-3 col-md-offset-3 col-md-4 col-sm-offset-2 col-sm-6 col-xs-offset-3 col-xs-8 \">{input} {label}</div>\n
 
 
 
@@ -60,7 +60,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
 
         <div class="form-group box-login-btn">
-            <div class="col-lg-offset-6 col-lg-5 col-md-offset-6 col-md-5">
+            <div class="col-lg-offset-6 col-lg-5 col-md-offset-6 col-md-5 col-xs-12">
                 <?= Html::submitButton('Ввійти', ['class' => 'btn btn-login', 'name' => 'login-button']) ?>
             </div>
         </div>
