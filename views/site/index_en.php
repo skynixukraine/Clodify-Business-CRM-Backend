890fa92ac01aa2f876935bdaba898dbb8575b670
Oleksii <alekseyyp@gmail.com>
2016-03-30 11:20:08 +0000

    Code English version

--- views/site/index_en.php
+++ views/site/index_en.php
@@ -5,21 +5,121 @@ use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<section class="container-fluid" id="about_skynix">
+    <div class="row">
+        <div class="col-lg-12 col-xs-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xs-12">
 
-    <div class="jumbotron">
-        <h1>Skynix System</h1>
-
-        <p class="lead">Skynix System is the system of management internal relationships.</p>
-
-        <p><a class="btn btn-lg btn-success" href="<?=Url::to(['contact'])?>">Request Access</a></p>
+                        <h1>ABOUT SKYNIX</h1>
+                    </div>
+                    <div class="col-lg-12 col-xs-12 about-txt1">
+                        Skynix Company is developing, strengthening and improving the software.
+                        The main goal of the company is to promote change and development of IT technologies.
+                    </div>
+                    <div class="col-lg-12 col-xs-12 about-txt2">
+                        We create innovative software solutions for businesses running.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</section>
 
-    <div class="body-content">
-
-        <div class="row">
+<section class="container" id="working_fields">
+    <div class="row">
+        <div class="col-lg-12 col-xs-12">
+            <h2>WE WORK IN THE FOLLOWING AREAS:</h2>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 web-network">
+            <div>Corporate<br>websites</div>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 hi">
+            <div>Blogs, forums, <br>Social Networking</div>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 phonegap">
+            <div>Mobile iOS and Android <br> Phonegap applications</div>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 ecommerce">
+            <div>eCommerce<br>solution</div>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 email">
+            <div>Themes and Templates<br>Email</div>
 
         </div>
+        <div class="col-lg-4 col-sm-4 col-xs-6 support">
+            <div>Technical<br>support</div>
 
+        </div>
     </div>
-</div>
+</section>
+
+<section class="container" id="technology">
+    <div class="row">
+        <div class="col-lg-12 col-xs-12">
+            <h3>
+                <span class="brand-name">SKYNIX</span>seeks to enrich, diversify and simplify your life
+                with the best technology.
+            </h3>
+        </div>
+        <div class="col-lg-12 col-xs-12 technology-txt">
+            We support a wide range of programming languages and represent some of our everyday technologies:
+        </div>
+    </div>
+    <div class="row scheme">
+        <div class="col-lg-12 col-xs-12 ">
+            <div class="backend">
+                <strong>BACKEND DEVELOPMENT</strong><br>
+                <ul>
+                    <li>PHP5</li>
+                    <li>Yii Framework 2</li>
+                    <li>Zend Framework 2</li>
+                    <li>Magento 2</li>
+                    <li>Wordpress</li>
+                </ul>
+            </div>
+            <div class="arrow-box">
+                <div class="arrow-left">
+                    <div class="dot"></div>
+                    <div class="angle"></div>
+                </div>
+                <div class="arrow-right">
+                    <div class="dot"></div>
+                    <div class="angle"></div>
+                </div>
+            </div>
+            <div class="frontend">
+                <strong>FRONTEND DEVELOPMENT</strong><br>
+                <ul>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>jQuery</li>
+                    <li>Sencha ExtJS</li>
+                    <li>AngularJS</li>
+                    <li>Twitter Bootstrap</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid" id="evaluation">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="box-evaluation">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-12 evaluation-txt-en">
+                            <p>You need to estimate the cost of the project or idea?</p>
+                        </div>
+                        <div class="col-lg-5 col-md-12">
+                            <a href="#">Free evaluation of the project</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
