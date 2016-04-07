06fddd71587ed7a10ff017b9f3e4a8618a7893e1
Lera <valeriya@skynix.co>
2016-04-07 08:42:04 +0300

    SI-181 fix

--- config/params.php
+++ config/params.php
@@ -6,5 +6,6 @@ return [
     'ua_site'           => 'http://ua.skynix.company',
     'en_site'           => 'http://skynix.company',
     'in_site'           => 'http://in.skynix.company',
-    'port'              => ""
+    'port'              => "",
+    'success'           => true
 ];
---
+++ mail/request.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lera
+ * Date: 06.04.16
+ * Time: 13:01
+ *
+ */
+ ?>
+<h1>Skynix - Requested a quote by, <?= $name?>,</h1><br>
+<?php echo $$websiteState. ' : ' . $websiteState . ',' ;?>
+<?php echo $$platform. ' : ' . $platform . ',';?>
+<?php echo $$services. ' : ' . $services . ',';?>
+<?php echo $$frontendPlatform. ' : ' . $frontendPlatform . ',';?>
+<?php echo $$backendPlatform. ' : ' . $backendPlatform . ',';?>
+<?php echo $$whenStart. ' : ' . $whenStart . ',';?>
+<?php echo $$budget . ' : ' . $budget . ',';?>
+<?php echo $$description . ' : ' . $description . ',';?>
+<?php echo $$file . ' : ' . $file . ',';?>
+<?php echo $$name . ' : ' . $name . ',';?>
+<?php echo $$email . ' : ' . $email . ',';?>
+<?php echo $$company . ' : ' . $company . ',';?>
+<?php echo $$country . ' : ' . $country . '.' ;?>
+
+
+
