e2a3fe6bc0c0579ee5ab62451ba868ff02c3f2eb
Lera <valeriya@skynix.co>
2016-04-07 16:39:49 +0300

    fix SI-181

--- mail/request.php
+++ mail/request.php
@@ -8,19 +8,19 @@
  */
  ?>
 <h1>Skynix - Requested a quote by, <?= $name?>,</h1><br>
-<?php echo $$websiteState. ' : ' . $websiteState . ',' ;?>
-<?php echo $$platform. ' : ' . $platform . ',';?>
-<?php echo $$services. ' : ' . $services . ',';?>
-<?php echo $$frontendPlatform. ' : ' . $frontendPlatform . ',';?>
-<?php echo $$backendPlatform. ' : ' . $backendPlatform . ',';?>
-<?php echo $$whenStart. ' : ' . $whenStart . ',';?>
-<?php echo $$budget . ' : ' . $budget . ',';?>
-<?php echo $$description . ' : ' . $description . ',';?>
-<?php echo $$file . ' : ' . $file . ',';?>
-<?php echo $$name . ' : ' . $name . ',';?>
-<?php echo $$email . ' : ' . $email . ',';?>
-<?php echo $$company . ' : ' . $company . ',';?>
-<?php echo $$country . ' : ' . $country . '.' ;?>
+<?php echo '$$websiteState.  : ' . $websiteState . ',' ;?>
+<?php echo '$$platform.  : ' . $platform . ',';?>
+<?php echo '$$services.  : ' . $services . ',';?>
+<?php echo '$$frontendPlatform.  : ' . $frontendPlatform . ',';?>
+<?php echo '$$backendPlatform.  : ' . $backendPlatform . ',';?>
+<?php echo '$$whenStart.  : ' . $whenStart . ',';?>
+<?php echo '$$budget .  : ' . $budget . ',';?>
+<?php echo '$$description .  : ' . $description . ',';?>
+<?php echo '$$file .  : ' . $file . ',';?>
+<?php echo '$$name .  : ' . $name . ',';?>
+<?php echo '$$email .  : ' . $email . ',';?>
+<?php echo '$$company .  : ' . $company . ',';?>
+<?php echo '$$country .  : ' . $country . '.' ;?>
 
 
 
