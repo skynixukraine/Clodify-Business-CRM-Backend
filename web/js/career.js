3f3f626f2707e8f1c54ad4a9f4d5189e4863df1b
Vitaliy <vitaliy@webais.company>
2016-04-11 14:14:27 +0000

    Career page

---
+++ web/js/career.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Vitaliy on 11.04.2016.
+ */
+
+var career = (function(){
+
+    var BtnReadMore,
+        articleShow,
+        htmlPage,
+        elem,
+        scrollRightPanel,
+        rightPanel;
+
+    return{
+
+        init: function(){
+            htmlPage        = $('body, html');
+            BtnReadMore = $('.read-more');
+            rightPanel = $(".right-panel");
+
+            BtnReadMore.click(function(event) {
+
+                event.preventDefault();
+                elem = $(this);
+                articleShow = $('.show-text');
+
+                if(articleShow.length != 0) {
+
+                    articleShow.closest("article").animate({height: '225px'}, {
+                        duration: 800
+                    });
+                    articleShow.removeClass('show-text');
+
+                }
+
+                elem.closest("article").animate({height: '100%'},{
+                    duration: 800
+                });
+
+                elem.closest("article").addClass('show-text');
+
+                setTimeout(scrolling, 900);
+
+                function scrolling(){
+
+                    htmlPage.animate({scrollTop: elem.closest("article").position().top},200);
+
+                }
+
+            });
+
+            $(document).scroll(function() {
+
+                scrollRightPanel = $(document).scrollTop();
+
+                console.log(scrollRightPanel);
+                if(scrollRightPanel > 240){
+
+                    rightPanel.addClass('fix-rightPanel');
+                }else{
+                    rightPanel.removeClass('fix-rightPanel');
+                }
+
+            });
+
+
+        }
+    }
+})();
+
+addEventListener("load", career.init);
+
+
--- web/js/layouts.js
+++ web/js/layouts.js
@@ -11,42 +11,40 @@ var LoginPage = (function(){
         init: function(){
 
             var header = $('.box-header-menu'),
-                scrollY;
+                scrollY,
+                heightW = $('.wrap').height(),
+                heightH = $('html').height();
 
-            function correctionPage(){
 
-                    var heightW = $('.wrap').height(),
-                    heightH = $('html').height();
+            if(heightW<heightH){
 
-                if(heightW<heightH){
+                header.css('box-shadow', 'none');
 
-                    header.css('box-shadow', 'none');
+            }else{
 
+                header.css('box-shadow', '0 2px 8px rgba(100, 192, 239, 0.37)');
+            }
+
+
+
+
+
+            $(document).scroll(function() {
+
+                scrollY = $(document).scrollTop();
+
+                if(scrollY > 100){
+
+                    header.addClass('fix-height');
                 }else{
-
-                    header.css('box-shadow', '0 2px 8px rgba(100, 192, 239, 0.37)');
+                    header.removeClass('fix-height');
                 }
 
-            }
+            });
 
-            function headerScroll(){
 
-                $(document).scroll(function() {
 
-                    scrollY = $(document).scrollTop();
 
-                    if(scrollY > 100){
-
-                        header.addClass('fix-height');
-                    }else{
-                        header.removeClass('fix-height');
-                    }
-
-                });
-            }
-            headerScroll();
-
-            correctionPage();
 
         }
     }
