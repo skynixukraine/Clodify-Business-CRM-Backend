42f6b1cc70e0c9eaaeab7f23437dfe98aa411957
Oleksii Prozhoga <alekseyyp@gmail.com>
2016-05-05 09:38:27 +0000

    SI-227

--- views/site/survey.php
+++ views/site/survey.php
@@ -69,6 +69,7 @@
      <div class = "loader">
             <img src="/img/495.gif" >
         </div>
+        <div></div>
   </body>
 
   <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
