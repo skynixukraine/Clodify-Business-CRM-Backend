403a416f396d4f47fe6d516efe177dabae904470
Anastasiia Kachura <nastyakachura@skynix.co>
2016-05-10 07:31:46 +0000

    SI-225

--- views/site/career_en.php
+++ views/site/career_en.php
@@ -133,4 +133,17 @@ $this->params['breadcrumbs'][] = $this->title;
 
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
+  <script>
+    $(function() {
+        localStorageModule.homePageProject();
+        localStorageModule.careerPageVacation();
+        localStorageModule.inputFunction();
+        localStorageModule.subjectLocal();
+ 
+})
+  </script>
+
+<?php $this->registerJsFile('/js/local-storage.js'); ?>
 <?php $this->registerJsFile('/js/career.js'); ?>
