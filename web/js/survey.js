750c150dffbc43779b53340294421f02370818c7
Anastasiia Kachura <nastyakachura@skynix.co>
2016-05-20 13:26:57 +0000

    SI-234 fixed bugs

--- web/js/survey.js
+++ web/js/survey.js
@@ -1,93 +1,93 @@
-var myModule = (function() {
-
-  var radioArr = $('input:radio[name=radio]');
-  var labelArr = $('input:radio[name= radio]').parent();
-  var surveySection =  $('.survey-wrap');
-  var formSent = $('.form-sent');
-  var textFormSentSuccess = "Form has been sent successfully";
-  var textFormSentError = "Unfortunately, the server is temporarily unavailable. Please try again later."
-  var progressBar = $(".loader");
+var SurveyModule = (function() {
 
   function showProgressBar() {
-   progressBar.css("display", "block");
+    $(".loader").css("display", "block");
   }
 
   function hideProgressBar() {
-   progressBar.css("display", "none");
+    $(".loader").css("display", "none");
   }
-
   
+
   return {
+    init: function() {
+        var myHtml = $("html"),
+        xhr = new XMLHttpRequest(),
+        radioArr = $('input:radio[name=radio]'),
+        labelArr = $('input:radio[name= radio]').parent(),
+        radioInput = $('input:radio');
 
-    tooltipLargeScreen : function() {
-      $(".tooltip-over").hover(
-        function() {
-          var tooltipLarge = $(this);
-          tooltipLarge.next(".tooltip-text").addClass("over");
-        } ,
-        function() {
-          var link = $(this);
-          window.setTimeout(function() {
-            $(link).next(".tooltip-text").removeClass('over');
-          }, 3000);
-        });
-
-    } ,
-
-    tooltipSmallScreen : function() {
-      $(".tooltip-over").click(function() {
-        var element = $(this);
-        if (element.next(".tooltip-text").hasClass("over")) {
-          element.next(".tooltip-text").removeClass('over');
-        } else {
-          element.next(".tooltip-text").addClass("over");
-        }
-      })
-    } ,
-
-    //Function for tracking changes in the radio buttons
-    changeFunction : function() {
-      for (var i = 0; i < radioArr.length; i++) {
-        radioArr[i].onchange = function() {
-          for (var k = 0; k < labelArr.length; k++) {
-            if (labelArr.hasClass("checked-radio")) {
-              labelArr.removeClass("checked-radio");
+      $('input:radio').click(function() {
+        radioArr.each(function() {
+          var thisRadio = $(this);
+          thisRadio.change(function() {
+            labelArr.each(function() {
+              thisPoint = $(this);
+              if (thisPoint.hasClass("checked-radio")) {
+                thisPoint.removeClass("checked-radio");
+              }
+            })
+            var thisLabel = thisRadio.parent();
+            if (this.checked) {
+              $("#submit").removeAttr("disabled");
+              thisLabel.addClass("checked-radio");
             }
-          }
-          var thisLabel = $(this).parent();
-          if (this.checked) {
-            $("#submit").removeAttr("disabled");
-            thisLabel.addClass("checked-radio");
-          }
 
-        }
+          })
+        })
+      })
+
+      if (myHtml.width() > 1170) {
+        $(".tooltip-over").hover(
+          function() {
+            var tooltipLarge = $(this);
+            tooltipLarge.next(".tooltip-text").addClass("over");
+          },
+          function() {
+            var link = $(this);
+            window.setTimeout(function() {
+              $(link).next(".tooltip-text").removeClass('over');
+            }, 3000);
+          });
+
       }
-    } ,
 
-    ajaxFormSubmit : function() {
+
+      if (myHtml.width() < 1170) {
+        $(".tooltip-over").click(function() {
+          var element = $(this);
+          if (element.next(".tooltip-text").hasClass("over")) {
+            element.next(".tooltip-text").removeClass('over');
+          } else {
+            element.next(".tooltip-text").addClass("over");
+          }
+        })
+      }
+
       $('form').submit(function(e) {
         e.preventDefault();
         showProgressBar();
         var data = $('form').serializeArray();
         $.ajax({
-          type: "POST" ,
-          url: "send.php" ,
-          data: data ,
-          dataType: "json" ,
-          success: function(){
-            hideProgressBar();
-            surveySection.slideUp();
-            formSent.slideDown().children("p").text(textFormSentSuccess);
-          } ,
-          error: function(){
-            hideProgressBar();
-            formSent.fadeIn().children("p").text(textFormSentError);
-            window.setTimeout(function(){
-              formSent.fadeOut();
-            } , 5000)
-          }
+          type: "POST",
+          url: "send.php",
+          data: data,
+          dataType: "html",
+          success: hideProgressBar()
         });
-     });
+
+        $(':input', 'form')
+          .not(':submit')
+          .val('')
+          .removeAttr('checked');
+      });
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          hideProgressBar();
+        }
+      }
+
     }
   }
 
