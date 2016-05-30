7b72db9f30901f342ceecda4582594c721885247
Lera <valeriya@skynix.co>
2016-05-30 11:20:14 +0300

    Support page

---
+++ models/SupportTicket.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "support_tickets".
+ *
+ * @property integer $id
+ * @property string $subject
+ * @property string $description
+ * @property integer $is_private
+ * @property integer $assignet_to
+ * @property string $status
+ * @property integer $client_id
+ * @property string $date_added
+ * @property string $date_completed
+ */
+class SupportTicket extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'support_tickets';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['description', 'status'], 'string'],
+            [['is_private', 'assignet_to', 'client_id'], 'integer'],
+            [['date_added', 'date_completed'], 'safe'],
+            [['subject'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'subject' => 'Subject',
+            'description' => 'Description',
+            'is_private' => 'Is Private',
+            'assignet_to' => 'Assignet To',
+            'status' => 'Status',
+            'client_id' => 'Client ID',
+            'date_added' => 'Date Added',
+            'date_completed' => 'Date Completed',
+        ];
+    }
+}
---
+++ views/support/index.php
@@ -0,0 +1,69 @@
+<?php
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View
+ */
+$this->title = 'Skynix Support';
+$this->registerJsFile('/js/bootstrap.min.js');
+$this->registerJsFile('/js/jquery-2.1.4.min.js');
+
+?>﻿
+<?php $form = ActiveForm::begin()?>
+<header>
+    <h1>Skynix Support</h1>
+   <!-- <nav class ="center_nav">
+        <ul class="nav nav-pills" >
+            <li>
+                <a href="#"><h3>вернуться на главную страницу</h3></a>
+            </li>
+
+        </ul>
+    </nav>-->
+</header>
+<div class="container-fluid">
+    <div class="row">
+        <section class="col-lg-6 col-lg-offset-3 sect3">
+            <article>
+                <!--<header>
+                    <h3>Подзаголовок</h3>
+                </header>-->
+                <p contenteditable="true">
+                <div class="form-group">
+                    <?php echo $form->field( $model, 'subject', [
+
+                        'options' => [
+                            'class'=>'supportticket-subject'
+
+                        ]
+                    ])->textInput(array('placeholder' => 'Enter the subject or your question'))->label( false );?>
+
+
+                </div><br>
+            </article>
+
+            <div class="col-md-12">
+                <?= Html::submitButton( Yii::t('app', 'Submit Request'), [
+                    'class' => 'btn btn-primary off-button',
+                    'style' => 'float: right; margin-top: 10px;',
+                    'disabled'  =>  'disabled',
+                    ])
+                ?>
+            </div>
+
+        </section>
+    </div>
+</div>
+<?php ActiveForm::end();?>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"; integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<script>
+    var supportTicket = $('.supportticket-subject');
+    supportTicket.change(function(){
+        var thisSup = $(this);
+        var saveButton = $('.off-button');
+        if(thisSup.val() == ""){
+            saveButton.removeAttr('disabled').css('background', '#337ab7');
+        }
+    })
+</script>
