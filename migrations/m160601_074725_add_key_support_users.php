8dc53000f17f43676ef77520d62798fb918e80bc
Lera <valeriya@skynix.co>
2016-06-06 17:11:17 +0300

    change migrate

--- migrations/m160601_074725_add_key_support_users.php
+++ migrations/m160601_074725_add_key_support_users.php
@@ -6,8 +6,8 @@ class m160601_074725_add_key_support_users extends Migration
 {
     public function up()
     {
-        $this->addForeignKey('fk_support_tickets_users', 'support_tickets', 'client_id', 'users', 'id');
-        $this->addForeignKey('fk_support_tickets_users', 'support_tickets', 'assignet_to', 'users', 'id');
+      /*  $this->addForeignKey('fk_support_tickets_users', 'support_tickets', 'client_id', 'users', 'id');
+        $this->addForeignKey('fk_support_tickets_users', 'support_tickets', 'assignet_to', 'users', 'id');*/
     }
 
     public function down()
