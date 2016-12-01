dbe467747667b40a1128bc5970ade2c72259f3db
milana <milana@skynix.co>
2016-12-01 18:06:22 +0200

    add styles for profile

--- views/profile/index.php
+++ views/profile/index.php
@@ -14,37 +14,37 @@ use app\models\ProjectDeveloper;
 use app\models\Report;
 use app\components\DateUtil;
 use kato\DropZone;
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/jquery.dataTables.min.js');
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/dataTables.bootstrap.min.js');
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/jquery.slimscroll.min.js');
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/modal.bootstrap.js');
-// $this->registerJsFile(Yii::$app->request->baseUrl.'/js/dropzone.js');
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/myprofile.js');
+
+
 $this->registerCssFile(Yii::$app->request->baseUrl.'/css/my-profile.css');
 $this->title                    = Yii::t("app", "My Profile");
 $this->params['breadcrumbs'][]  = $this->title;
 $this->params['menu'] = [
 ];
 
-var_dump($user->first_name);
 ?>
-<?php $form = ActiveForm::begin(); ?>
-<div class="tab-content">
-    <div id="tab_1" class="tab-pane active col-xs-12">
-        <div class="my-profile-form col-sm-12">
-            <div class="row">
-                <fieldset class = "col-sm-6">
-                    <?php /** @var $model User */?>
-                    <?php echo $form->field( $user, 'first_name' )->textInput(['readonly' => true]);?>
-                    <?php echo $form->field( $user, 'last_name' )->textInput(['readonly' => true]);?>
-                </fieldset>
-                <fieldset class = "col-sm-6">
-                    <?php echo $form->field( $user, 'tags' )->textInput(['readonly' => true])->label( 'Your primary skills' );?>
-                    <?php echo $form->field( $user , 'about')->textArea(['readonly' => true]);?>
-                </fieldset>
-            </div>
+<div class="container profile">
+    <v class="row">
+        <div class="col-lg-12">
+            <h1><?=Yii::t('app', 'Profile')?></h1>
         </div>
+        <?php $form = ActiveForm::begin(); ?>
+                <div class="my-profile-form col-lg-12">
+                    <div class="row">
+                        <fieldset class = "col-lg-6">
+                            <?php /** @var $model User */?>
+                            <?php echo $form->field( $user, 'first_name' )->textInput(['readonly' => true]);?>
+                            <?php echo $form->field( $user, 'last_name' )->textInput(['readonly' => true]);?>
+                        </fieldset>
+                        <fieldset class = "col-lg-6">
+                            <?php echo $form->field( $user, 'tags' )->textInput(['readonly' => true])->label( 'Your primary skills' );?>
+                            <?php echo $form->field( $user , 'about')->textArea(['readonly' => true]);?>
+                        </fieldset>
+                    </div>
+                </div>
+        <?php $form = ActiveForm::end(); ?>
     </div>
 </div>
 
 
+</div>
