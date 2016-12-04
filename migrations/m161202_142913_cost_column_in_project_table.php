a7659d9b055f664510c673582def88d2b46aea24
milana <kostivmilana@gmail.com>
2016-12-04 23:38:44 +0200

    Migration update that fills cost for existed reports

--- migrations/m161202_142913_cost_column_in_project_table.php
+++ migrations/m161202_142913_cost_column_in_project_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use yii\db\Migration;
 
 class m161202_142913_cost_column_in_project_table extends Migration
@@ -7,8 +6,22 @@ class m161202_142913_cost_column_in_project_table extends Migration
     public function up()
     {
         $this->addColumn('projects', 'cost', $this->float());
+		$this->db->createCommand('
+			UPDATE projects p
+			LEFT JOIN (SELECT
+				reports.project_id prid,
+				SUM(reports.hours * (users.salary / 168)) AS cost
+			  FROM reports
+				INNER JOIN projects
+				  ON reports.project_id = projects.ID
+				INNER JOIN users
+				  ON reports.user_id = users.ID
+			  WHERE reports.is_delete = 0
+			  AND users.is_delete = 0
+			  GROUP BY project_id) AS costs
+			  ON p.ID = costs.prid
+			SET p.cost = costs.cost')->execute();
     }
-
     public function down()
     {
         echo "m161202_142913_cost_column_in_project_table cannot be reverted.\n";
