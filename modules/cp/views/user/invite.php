a1e94b6436216cd4ab8e899b5646d2ae7195ac18
milana <milana@skynix.co>
2016-12-12 17:11:40 +0200

    invite form

--- modules/cp/views/user/invite.php
+++ modules/cp/views/user/invite.php
@@ -26,6 +26,18 @@ $this->params['menu'] = [
         /** @var $model User */ ?>
     <div class="row">
         <div class="col-md-6 box box-primar box-body">
+            <div>
+                <?php echo $form->field($model, 'role')->dropDownList([
+                    User::ROLE_ADMIN    => 'ADMIN',
+                    User::ROLE_DEV      => 'DEV',
+                    User::ROLE_FIN      => 'FIN',
+                    User::ROLE_CLIENT   => 'CLIENT',
+                    User::ROLE_PM       => 'PM',
+                    User::ROLE_SALES    => 'SALES',
+                ],
+                    ['prompt' => 'Choose...']
+                );?>
+            </div>
             <?php echo $form->field( $model, 'first_name')
                 ->textInput( [ 'class'        => 'form-control',
                                'placeholder'  => 'First name',
@@ -35,6 +47,10 @@ $this->params['menu'] = [
                 ->textInput( ['class'   => 'form-control',
                               'placeholder'   => 'Last name',
                 ])-> label( 'Last name' );?>
+            <?php echo $form->field( $model, 'middle_name')
+                ->textInput( ['class'   => 'form-control',
+                    'placeholder'   => 'Middle name',
+                ])-> label( 'Middle name' );?>
             <?php echo $form->field( $model, 'company')
                 ->textInput( [ 'class'        => 'form-control',
                     'placeholder'  => 'Company',
@@ -45,19 +61,8 @@ $this->params['menu'] = [
                               'placeholder'   => 'Enter email',
                               'type'          => 'email',
                 ])-> label( 'Email' );?>
-
-            <div>
-                <?php echo $form->field($model, 'role')->dropDownList([
-                    User::ROLE_ADMIN    => 'ADMIN',
-                    User::ROLE_DEV      => 'DEV',
-                    User::ROLE_FIN      => 'FIN',
-                    User::ROLE_CLIENT   => 'CLIENT',
-                    User::ROLE_PM       => 'PM',
-                    User::ROLE_SALES    => 'SALES',
-                ],
-                  ['prompt' => 'Choose...']
-                );?>
-            </div>
+            <?php echo $form->field( $model, 'phone')->textInput(["class" => "form-control"])->label( 'Phone' );?>
+            <?php echo $form->field( $model, 'salary')->textInput(["class" => "form-control"])->label( 'Salary' );?>
             <div>
                 <?= Html::submitButton( Yii::t('app', 'Invite'), ['class' => 'btn btn-primary']) ?>
             </div>
