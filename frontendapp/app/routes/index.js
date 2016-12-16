1b67abe1817fb8ff7fbf947e90e5302b22925f48
Dakal__Roma <roman@skynix.co>
2016-12-16 16:57:36 +0200

    SI-423-coding-block-1-intro-2

--- frontendapp/app/routes/index.js
+++ frontendapp/app/routes/index.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 
-let description = 'Skynix Ukraine is a software development company founded by the';
-let slides = [
-  {
-    title: '',
-    description: '',
-    img: ''
-  }
-];
+let slides = {
+  description: 'Skynix Ukraine is a software development company founded by the technical experts in the IT field with the ultimate customer satisfaction and innovative approach in mind.',
+  img: 'assets/img/home-page/section-1/Logo.png'
+};
 
 export default Ember.Route.extend({
   model() {
     return {
-      description: description,
       slides: slides
     }
   }
