604244c70adf9449757e30b3bf75456112a7a585
Dakal__Roma <roman@skynix.co>
2016-12-20 17:38:32 +0200

    SI-425-coding-block-3-slider

--- frontendapp/app/components/slider-touch-component.js
+++ frontendapp/app/components/slider-touch-component.js
@@ -30,12 +30,13 @@ export default Ember.Component.extend({
 
       let sliderItemNode = element.find(".slider__list"),
         prevSliderNode = element.find('.slider__pager_previous'),
-        nextSliderNode = element.find('.slider__pager_next');
+        nextSliderNode = element.find('.slider__pager_next'),
+        paginationNode = element.find('.slider__pagination');
+
 
       let currentSlideIndex = 0,
         imagesCount = sliderItemNode.children().length,
         slideSize = sliderItemNode.outerWidth();
-      // console.log(imagesCount);
 
       this.prevSlide = function () {
         if (currentSlideIndex === 0) {
@@ -43,7 +44,6 @@ export default Ember.Component.extend({
           return;
         }
         currentSlideIndex--;
-        console.log(currentSlideIndex);
       };
 
       this.nextSlide = function () {
@@ -52,13 +52,15 @@ export default Ember.Component.extend({
           return;
         }
         currentSlideIndex++;
-        console.log(currentSlideIndex);
       };
 
+
       this.__render = function () {
-
         sliderItemNode.css({"marginLeft": -(currentSlideIndex * slideSize)});
+        paginationNode.find('.active').removeClass('active');
+        let elem = paginationNode.children()[currentSlideIndex];
 
+        $(elem).find('a').addClass('active');
 
       };
       prevSliderNode.on('click', function (e) {
@@ -72,32 +74,71 @@ export default Ember.Component.extend({
         __self.nextSlide();
         __self.__render();
       });
+      paginationNode.on('click', function(e) {
+        e.preventDefault();
 
+        let link = e.target;
+
+        if (link.tagName !== 'A') { return; }
+
+        currentSlideIndex = +link.dataset['slider__item'];
+
+        __self.__render();
+      });
 
       let mousePageX,
-          mouseMove,
-          size;
+        mouseMove,
+        size,
+        touchPageX,
+        touchMove,
+        touchSize;
 
-      sliderItemNode.on('touchstart mousedown', function (e) {
-        mousePageX = e.pageX;
-        event.preventDefault();
-        // console.log(mousePageX);
-        sliderItemNode.on('touchmove mousemove', function (e) {
-          mouseMove = e.pageX;
-        });
-      });
-      sliderItemNode.on('touchend mouseup', function () {
-        sliderItemNode.off('touchmove mousemove');
-        if(mouseMove > 800){
+      this.swipeSlider = function (item) {
+        if (item > 60) {
           __self.nextSlide();
           __self.__render();
-        }else{
+        }
+        if (item < -60) {
           __self.prevSlide();
           __self.__render();
         }
+      };
+      sliderItemNode.on('touchstart mousedown', function (e) {
+        e.preventDefault();
+        if (e.type == "touchstart") {
+          touchPageX = e.changedTouches[0].pageX;
+        } else {
+          mousePageX = e.pageX;
+        }
+        sliderItemNode.on('touchmove mousemove', function (e) {
+          if (e.type == "touchmove") {
+            touchMove = e.changedTouches[0].pageX;
+          } else {
+            mouseMove = e.pageX;
+          }
+        });
+      });
+      sliderItemNode.on('touchend mouseup', function (e) {
+        if (e.type == "touchend") {
+          touchSize = touchPageX - touchMove;
+          if (touchSize == 0) {
+            sliderItemNode.off('touchmove');
+          } else {
+            __self.swipeSlider(touchSize);
+          }
+        } else {
+          size = mousePageX - mouseMove;
+          if (size == 0) {
+            sliderItemNode.off('mousemove');
+          } else {
+            __self.swipeSlider(size);
+          }
+        }
+
       });
     }
 
     let slider = new Slider();
+
   }
 });
