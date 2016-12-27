a6e4915e1e651e40ec0ff984aa97bee223ba0b21
Oleksii Prozhoga <alekseyy@gmail.com>
2016-12-27 16:04:54 +0200

    Reorginized backend app

--- deployer.sh
+++ deployer.sh
@@ -2,21 +2,8 @@
 echo "Pulling data from the repo \n";
 git pull origin develop
 
-cd frontendapp
-
-npm install
-
-bower install
-
-ember build --environment development
-
-cd ..
-
 echo "Updating composer \n";
 composer install
 
 echo "\n\n Appling migrations \n";
 ./yii migrate --interactive=0
-
-echo "9. Let write to dist/assets, chmod -R 0777 dist/assets \n"
-chmod -R 0777 web/backend/assets
--- frontendapp/README.md
+++
@@ -1,4 +0,0 @@
-Project board http://colourways.co.uk:8080/secure/RapidBoard.jspa?rapidView=3
-Dev site http://ecs.dev.skynix.co/
-
-Emberjs based project
