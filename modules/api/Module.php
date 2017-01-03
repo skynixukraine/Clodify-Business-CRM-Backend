d71322203d7926e1c5af75d068d96e30e869a3e3
milana <milana@skynix.co>
2017-01-03 20:36:26 +0200

    add module.php class

--- modules/api/Module.php
+++ modules/api/Module.php
@@ -1,6 +1,29 @@
 <?php
+
+namespace app\modules\api;
+use Yii;
+
 /**
- * Created by Skynix Team
- * Date: 03.01.17
- * Time: 18:50
+ * api module definition class
  */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\api\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here
+        //Yii::setAlias('viewModel', Yii::getAlias("@app") . '/modules/api/view-models');
+
+        // http://www.yiiframework.com/doc-2.0/guide-rest-authentication.html
+        //Yii::$app->user->enableSession = false;
+       // Yii::$app->user->loginUrl = null;
+    }
+}
