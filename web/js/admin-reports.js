31f179492e8f74a155fe88d0cca0fd597d45a93a
Korets Dmytro <korets.web@gmail.com>
2017-02-02 14:01:12 +0200

    SI-701 Long reports modifying grid to unreadable format

--- web/js/admin-reports.js
+++ web/js/admin-reports.js
@@ -110,7 +110,18 @@ var adminReportModule = (function() {
                     "targets"   : 1,
                     "orderable" : true,
                     "render" : function (data, type, row) {
-                        return row[1].replace(/(<([^>]+)>)/ig,"");
+                        row[1] = row[1].replace(/(<([^>]+)>)/ig,"");
+                        if (row[1].length >= 35) {
+                            multiplier = Math.ceil(row[1].length/35);
+                            task = '';
+                            for (i = 0; i <= multiplier; i++) {
+                                task += row[1].substr(i*35,35) + '<br>';
+                            }
+                            return task;
+                        }
+
+                        return row[1];
+
                     }
                 }
             ], index=1;
