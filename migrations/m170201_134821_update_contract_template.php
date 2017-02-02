057251b07931c78342c0bf2bcd591bc04292c866
Vitaliy <vitalii@skynix.co>
2017-02-02 13:53:43 +0000

    SI-626 Some comments on PDF contract template

--- migrations/m170201_134821_update_contract_template.php
+++ migrations/m170201_134821_update_contract_template.php
@@ -8,60 +8,60 @@ class m170201_134821_update_contract_template extends Migration
     {
         $description = '<table width="570" style="max-width: 570px; margin-left: auto; margin-right: auto; border-collapse: collapse;">
 
-    <tr style = "height: 100%; box-sizing: border-box; border-collapse: collapse;">
-        <td width="285" style =" vertical-align: top; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px; font-family:\'Times New Roman\';font-size:10px; padding: 5px;">
-            <table width="285" style="margin:0;border-collapse: collapse;border: 0;">
-                <tr>
-                    <td align="center" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">Виконавець</td>
-                </tr>
-                <tr>
-                    <td align="justify" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">
-                        <p><span style="color: #ffffff;">.</span></p>
-                        <p>Бенефіциар: <strong>Прожога Олексій Юрійович</strong></p>
-                        <p>Адреса бенефіциара: <strong>UA 08294</strong></p>
-                        <p><strong>Київська обл., м. Буча</strong></p>
-                        <p><strong>вул. Тарасiвська д.8а кв.128</strong></p>
-                        <p>Рахунок бенефіциара: <strong>26002057002108</strong></p>
-                        <p>SWIFT код: <strong>PBANUA2X</strong></p>
-                        <p>Банк бенефіциара: <strong>Privatbank, Dnipropetrovsk, Ukraine</strong></p>
-                        <p>IBAN Code: <strong>UA323515330000026002057002108</strong></p>
-                        <p>Банк-корреспондент: <strong>JP Morgan</strong></p>
-                        <p><strong>Chase Bank,New York ,USA</strong></p>
-                        <p>Рахунок у банку-кореспонденту: <strong>001-1-000080</strong></p>
-                        <p>SWIFT код кореспондента: <strong>CHASUS33</strong></p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-        <td width="284" style =" vertical-align: top; border-collapse: collapse; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; padding: 5px; font-family:\'Times New Roman\'; font-size:10px; padding: 5px;">
-            <table width="284" style="margin:0;border-collapse: collapse;border: 0;">
-                <tr>
-                    <td align="center" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">Contractor</td>
-                </tr>
-                <tr>
-                    <td align="justify" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">
-                        <p><span style="color: #ffffff;">.</span></p>
-                        <p>BENEFICIARY: <strong>Prozhoga Oleksii Yuriyovich</strong></p>
-                        <p>BENEFICIARY ADDRESS: <strong>UA 08294 Kiyv,</strong></p>
-                        <p><strong>Bucha, Tarasivska st. 8a/128</strong></p>
-                        <p><span style="color: #ffffff;">.</span></p>
-                        <p>BENEFICIARY ACCOUNT: <strong>26002057002108</strong></p>
-                        <p>SWIFT CODE: <strong>PBANUA2X</strong></p>
-                        <p>BANK OF BENEFICIARY: <strong>Privatbank,</strong></p>
-                        <p><strong>Dnipropetrovsk, Ukraine</strong></p>
-                        <p>IBAN Code: <strong>UA323515330000026002057002108</strong></p>
-                        <p>CORRESPONDENT BANK: <strong>JP Morgan</strong></p>
-                        <p><strong>Chase Bank,New York ,USA</strong></p>
-                        <p>CORRESPONDENT ACCOUNT: <strong>001-1-000080</strong></p>
-                        <p>SWIFT Code of correspondent bank: <strong>CHASUS33</strong></p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
+<tr style = "height: 100%; box-sizing: border-box; border-collapse: collapse;">
+    <td width="285" style =" vertical-align: top; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px; font-family:\'Times New Roman\';font-size:10px; padding: 5px;">
+        <table width="285" style="margin:0;border-collapse: collapse;border: 0;">
+            <tr>
+                <td align="center" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">Виконавець</td>
+            </tr>
+            <tr>
+                <td align="justify" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">
+                    <p><span style="color: #ffffff;">.</span></p>
+                    <p>Бенефіциар: <strong>Прожога Олексій Юрійович</strong></p>
+                    <p>Адреса бенефіциара: <strong>UA 08294</strong></p>
+                    <p><strong>Київська обл., м. Буча</strong></p>
+                    <p><strong>вул. Тарасiвська д.8а кв.128</strong></p>
+                    <p>Рахунок бенефіциара: <strong>26002057002108</strong></p>
+                    <p>SWIFT код: <strong>PBANUA2X</strong></p>
+                    <p>Банк бенефіциара: <strong>Privatbank, Dnipropetrovsk, Ukraine</strong></p>
+                    <p>IBAN Code: <strong>UA323515330000026002057002108</strong></p>
+                    <p>Банк-корреспондент: <strong>JP Morgan</strong></p>
+                    <p><strong>Chase Bank,New York ,USA</strong></p>
+                    <p>Рахунок у банку-кореспонденту: <strong>001-1-000080</strong></p>
+                    <p>SWIFT код кореспондента: <strong>CHASUS33</strong></p>
+                </td>
+            </tr>
+        </table>
+    </td>
+    <td width="284" style =" vertical-align: top; border-collapse: collapse; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; padding: 5px; font-family:\'Times New Roman\'; font-size:10px; padding: 5px;">
+        <table width="284" style="margin:0;border-collapse: collapse;border: 0;">
+            <tr>
+                <td align="center" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">Contractor</td>
+            </tr>
+            <tr>
+                <td align="justify" style="margin: 0; font-family:\'Times New Roman\';font-size:10px;">
+                    <p><span style="color: #ffffff;">.</span></p>
+                    <p>BENEFICIARY: <strong>Prozhoga Oleksii Yuriyovich</strong></p>
+                    <p>BENEFICIARY ADDRESS: <strong>UA 08294 Kiyv,</strong></p>
+                    <p><strong>Bucha, Tarasivska st. 8a/128</strong></p>
+                    <p><span style="color: #ffffff;">.</span></p>
+                    <p>BENEFICIARY ACCOUNT: <strong>26002057002108</strong></p>
+                    <p>SWIFT CODE: <strong>PBANUA2X</strong></p>
+                    <p>BANK OF BENEFICIARY: <strong>Privatbank,</strong></p>
+                    <p><strong>Dnipropetrovsk, Ukraine</strong></p>
+                    <p>IBAN Code: <strong>UA323515330000026002057002108</strong></p>
+                    <p>CORRESPONDENT BANK: <strong>JP Morgan</strong></p>
+                    <p><strong>Chase Bank,New York ,USA</strong></p>
+                    <p>CORRESPONDENT ACCOUNT: <strong>001-1-000080</strong></p>
+                    <p>SWIFT Code of correspondent bank: <strong>CHASUS33</strong></p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
 </table>';
-        $content = '
-        <!doctype html>
+$content = '
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -270,27 +270,27 @@ class m170201_134821_update_contract_template extends Migration
 </table>
 ';
 
-    $this->update('contract_templates', ['content' => $content], 'name=:name', [':name' => 'Default template']);
-    $this->update('payment_methods', ['description' => $description], 'name=:name', [':name' => 'Default payment method']);
+$this->update('contract_templates', ['content' => $content], 'name=:name', [':name' => 'Default template']);
+$this->update('payment_methods', ['description' => $description], 'name=:name', [':name' => 'Default payment method']);
 
 
-    }
+}
 
-    public function down()
-    {
-        echo "m170201_134821_update_contract_template cannot be reverted.\n";
+public function down()
+{
+echo "m170201_134821_update_contract_template cannot be reverted.\n";
 
-        return false;
-    }
+return false;
+}
 
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
+/*
+// Use safeUp/safeDown to run migration code within a transaction
+public function safeUp()
+{
+}
 
-    public function safeDown()
-    {
-    }
-    */
+public function safeDown()
+{
+}
+*/
 }
--- modules/cp/views/contract/contractPDF.php
+++ modules/cp/views/contract/contractPDF.php
@@ -86,10 +86,10 @@ echo str_replace($search, $replace, $contract_template->content) . $payment_temp
 <table width="570" style="max-width: 570px; margin-left: auto; margin-right: auto; border-collapse: collapse;">
 
     <tr style = "height: 100%; box-sizing: border-box; border-collapse: collapse;">
-        <td width="285" style =" vertical-align: top; border: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px; font-family:'Times New Roman';font-size:10px;">
+        <td width="285" style =" vertical-align: top; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black;  height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px; font-family:'Times New Roman';font-size:10px;">
             <?=$user->bank_account_ua?>
         </td>
-        <td width="284" style =" vertical-align: top; border-collapse: collapse; border: 1px solid black; height: 100%; box-sizing: border-box; padding: 5px; font-family:'Times New Roman';font-size:10px;">
+        <td width="284" style =" vertical-align: top; border-collapse: collapse; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; padding: 5px; font-family:'Times New Roman';font-size:10px;">
             <?=$user->bank_account_en?>
         </td>
     </tr>
@@ -101,11 +101,21 @@ if ($user->sing) :?>
 
 <table width="570" style=" margin-left: auto; margin-right: auto; border-collapse: collapse;">
     <tr style = "height: 100%; box-sizing: border-box; border-collapse: collapse; ">
-        <td width="285" style =" vertical-align: top; border: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px;">
+        <td width="285" style =" vertical-align: top; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px;">
             <img src="<?=$user->getUserSingPath()?>" style="max-width: 120px;">
         </td>
-        <td width="284" style =" vertical-align: top; border: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px;">
+        <td width="284" style =" vertical-align: top; border-left: 1px solid black; border-right: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px;">
             <img src="<?=$user->getUserSingPath()?>" style="max-width: 120px;">
+        </td>
+    </tr>
+    <tr style = "height: 100%; box-sizing: border-box; border-collapse: collapse; ">
+        <td width="285" style ="width: 285px; vertical-align: top; border-bottom: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; border-left: 1px solid black; border-right: 0px solid black;height: 100%; box-sizing: border-box; border-collapse: collapse; padding: 5px; font-family:\'Times New Roman\';font-size:10px;">
+            <p style="color: #ffffff;">_________________/________</p>
+            <p>Підпис</p>
+        </td>
+        <td width="284" style ="width: 284px; vertical-align: top; border-collapse: collapse; border-bottom: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; height: 100%; box-sizing: border-box; padding: 5px; font-family:\'Times New Roman\';font-size:10px;">
+            <p style="color: #ffffff;">_________________/________</p>
+            <p>Signature</p>
         </td>
     </tr>
 </table>
