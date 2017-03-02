955e31c49de3cd75f78a0a3c17085d5f2f67c9c9
milana <milana@skynix.co>
2017-03-02 16:22:24 +0200

    SI-841-215-report-dateperiod-list

--- config/route.php
+++ config/route.php
@@ -1,10 +1,6 @@
 <?php
 $API = 'api';
 return [
-    [
-        'pattern'   => $API . '/reports/date_period',
-        'route'     => $API . '/reports/date-period'
-    ],
     // General rules
     $API . '/<controller>'              => $API . '/<controller>',
     $API . '/<controller>/<action>'     => $API . '/<controller>/<action>',
--- modules/api/view-models/DatePeriod.php
+++ modules/api/view-models/DatePeriod.php
@@ -8,16 +8,10 @@
 
 namespace viewModel;
 
-use app\components\DataTable;
-use app\models\Project;
-use app\models\ProjectCustomer;
-use app\models\ProjectDeveloper;
+
 use app\models\Report;
-use app\models\User;
-use app\modules\api\components\Api\Processor;
 use Yii;
-use DateTime;
-use app\modules\api\components\SortHelper;
+
 
 
 class DatePeriod extends ViewModelAbstract
