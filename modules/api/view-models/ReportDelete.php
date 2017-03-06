b44c927517951ad998e5408f8822d30af88fa329
milana <milana@skynix.co>
2017-03-06 11:04:38 +0200

    SI-840-214-delete-reports-data

--- modules/api/view-models/ReportDelete.php
+++ modules/api/view-models/ReportDelete.php
@@ -10,7 +10,7 @@ namespace viewModel;
 use app\models\Report;
 use app\models\User;
 use app\models\Project;
-
+use app\modules\api\components\Api\Processor;
 use Yii;
 
 /**
@@ -27,18 +27,22 @@ class ReportDelete extends ViewModelAbstract
         if($model) {
             //User can delete only own report
             if($model->user_id == ($this->getAccessTokenModel()->user_id) ) {
-            if ($model->invoice_id == null) {
-                $model->is_delete = 1;
-                if ($model->save(true, ['is_delete'])) {
-                    $user = User::findOne($model->user_id);
-                    $project = Project::findOne($model->project_id);
-                    $project->cost -= round($model->hours * ($user->salary / Report::SALARY_HOURS), 2);
-                    $project->total_logged_hours -= $model->hours;
-                    $project->save(true, ['total_logged_hours', 'cost']);
+                if ($model->invoice_id == null) {
+                    $model->is_delete = 1;
+                    if ($model->save(true, ['is_delete'])) {
+                        $user = User::findOne($model->user_id);
+                        $project = Project::findOne($model->project_id);
+                        $project->cost -= round($model->hours * ($user->salary / Report::SALARY_HOURS), 2);
+                        $project->total_logged_hours -= $model->hours;
+                        $project->save(true, ['total_logged_hours', 'cost']);
+                    } else {
+                        $this->addError(Processor::CODE_INSERT_ERROR, Yii::t('app','Report wasn\'t deleted'));
+                    }
+                }  else {
+                    $this->addError(Processor::ERROR_PARAM, Yii::t('app','Invoice was created for this report'));
                 }
-            }
             } else {
-                $this->addError('invoice', Yii::t('app','Invoice was created for this report'));
+                $this->addError(Processor::ERROR_PARAM, Yii::t('app','You can delete only own reports'));
             }
         } else {
             $this->addError('id', Yii::t('app','Such report is not existed'));
