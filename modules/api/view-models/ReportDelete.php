95bfaec9932b36a594a822983f00168b71005134
milana <milana@skynix.co>
2017-03-06 10:38:15 +0200

    SI-840-214-delete-reports-data

--- modules/api/view-models/ReportDelete.php
+++ modules/api/view-models/ReportDelete.php
@@ -25,15 +25,18 @@ class ReportDelete extends ViewModelAbstract
         $id = Yii::$app->request->getQueryParam('id');
         $model   = Report::findOne($id);
         if($model) {
+            //User can delete only own report
+            if($model->user_id == ($this->getAccessTokenModel()->user_id) ) {
             if ($model->invoice_id == null) {
                 $model->is_delete = 1;
                 if ($model->save(true, ['is_delete'])) {
-                    $user    = User::findOne($model->user_id);
+                    $user = User::findOne($model->user_id);
                     $project = Project::findOne($model->project_id);
                     $project->cost -= round($model->hours * ($user->salary / Report::SALARY_HOURS), 2);
                     $project->total_logged_hours -= $model->hours;
                     $project->save(true, ['total_logged_hours', 'cost']);
                 }
+            }
             } else {
                 $this->addError('invoice', Yii::t('app','Invoice was created for this report'));
             }
