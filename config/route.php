8dfb4ba6d36b8a6871a289c59180dc5254ba736f
Dmitry Korets <korets.web@gmail.com>
2017-03-13 09:52:18 +0200

    2.2.4.Edit_user API(edit spaces in route.php)

--- config/route.php
+++ config/route.php
@@ -8,7 +8,7 @@ return [
     'PUT '      . $API . '/users/<id:\d+>/deactivate' => $API . '/users/deactivate',
     'GET '      . $API . '/users/<id:\d+>'            => $API . '/users/view',
     'DELETE '   . $API . '/users/<id:\d+>'            => $API . '/users/delete',
-    'PUT    '   . $API . '/users/<id:\d+>'            => $API . '/users/edit',
+    'PUT '      . $API . '/users/<id:\d+>'            => $API . '/users/edit',
     // General rules
     $API . '/<controller>'              => $API . '/<controller>',
     $API . '/<controller>/<action>'     => $API . '/<controller>/<action>',
