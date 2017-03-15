8734ac707105cecafda5bee65c7edaac6f9e4aa3
Korets Dmytro <korets.web@gmail.com>
2017-03-15 12:18:14 +0200

    SI-858 2.2.5 Delete Users Data(add delete relations action between developer and their projects)

--- modules/api/view-models/UserDelete.php
+++ modules/api/view-models/UserDelete.php
@@ -9,6 +9,7 @@ namespace viewModel;
 
 
 use app\models\ProjectCustomer;
+use app\models\ProjectDeveloper;
 use app\models\User;
 use app\modules\api\components\Api\Processor;
 use Yii;
@@ -30,13 +31,20 @@ class UserDelete extends ViewModelAbstract
             $model->is_delete = 1;
             $model->save(true, ['is_delete']);
 
-            $projectCustomer = ProjectCustomer::find()->where(['user_id' => $id])->all();
+
             //When we are deleting client, we should delete all relations between this customer and his projects
-            if( $projectCustomer ) {
+            // Same with developers
+            if( $projectCustomer = ProjectCustomer::find()->where(['user_id' => $id])->all() ) {
                 foreach ($projectCustomer as $projectCustomerRelation) {
                     $projectCustomerRelation->delete();
                 }
+
+            } elseif ($projectDeveloper = ProjectDeveloper::find()->where(['user_id' => $id])->all()) {
+                foreach ($projectDeveloper as $projectDeveloperRelation) {
+                    $projectDeveloperRelation->delete();
+                }
             }
+
         } else {
             return $this->addError(Processor::ERROR_PARAM, 'You have no access for this action');
         }
