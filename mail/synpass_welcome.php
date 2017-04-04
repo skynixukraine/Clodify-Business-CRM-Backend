e50bdcc1dacc6895ea3caa6a27a06203a0f888bc
Oleksii Prozhoga <alekseyy@gmail.com>
2017-04-04 15:51:48 +0300

    SYN-27 Email template - pre-release new subscriber

---
+++ mail/synpass_welcome.php
@@ -0,0 +1,20 @@
+<img src="https://synpass.pro/img/logo.png" alt="Synpass Logo" /><br>
+
+Hey there,<br><br>
+
+First off, I’d like to extend a warm welcome and ‘thank you’ for expressing your interest in Synpass. I recognize that your time is valuable and I can't wait to show you what we've been working on.
+<br><br>
+I'm not allowed to tell you much about our new product yet... But now that you subscribed, you'll be among the first few people to be handed a key and try it! And once you do, we wholeheartedly believe Synpass will change the way you manage your personal data once and forever.
+<br><br>
+In the meantime, I’d love to hear from you about how you found us, what do you think our product will be, and what is your digital life currently missing. So long as you reply to this email, I promise I will too.
+<br><br>
+Again, welcome!
+<br><br>
+Best,
+<br><br>
+Kristina Antypova<br>
+Co-founder at Synpass
+<br><br>
+If you no longer wish to receive our emails, click <a href="mailto:admin@synpass.pro?subject=Unsubscribe Me">unsubscribe.
+
+
