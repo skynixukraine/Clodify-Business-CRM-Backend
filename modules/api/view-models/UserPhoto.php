2874bf4777dccc66a19cb03618bd3fc0ae2c14ec
Алексей Грибан <alexey.vnu@gmail.com>
2017-04-12 14:38:27 +0000

    Merged in SI-904-228-attach-users-photo-fix (pull request #302)
    SI-904 228 Attach Users Photo Fix
    
    Approved-by: Milana Kostiv <milana@skynix.co>
    Approved-by: Oleksii Prozhoga <alekseyyp@gmail.com>

---
+++ modules/api/view-models/UserPhoto.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by Skynix Team
+ * Date: 11.04.17
+ * Time: 18:01
+ */
+
+namespace viewModel;
+
+use Yii;
+use yii\helpers\FileHelper;
+use yii\web\UploadedFile;
+use app\models\User;
+
+/**
+ * Class UserPhoto
+ * @package viewModel
+ */
+class UserPhoto extends ViewModelAbstract
+{
+
+    public $model;
+
+    public function define()
+    {
+        $fileName = 'photo';
+        $file = \yii\web\UploadedFile::getInstanceByName($fileName);
+        $this->model->photo = $file;
+        if ($this->validate()) {
+            $uploadPath = Yii::getAlias('@app') . '/data/' . Yii::$app->user->id . '/';
+            if (!file_exists($uploadPath)) {
+                mkdir($uploadPath, 0777, true);
+                chmod($uploadPath, 0777);
+            }
+            $uploadPath = $uploadPath . 'photo/';
+            if (!file_exists($uploadPath)) {
+                mkdir($uploadPath, 0777, true);
+                chmod($uploadPath, 0777);
+            }
+
+            if ($file->size <= 5242880) { // 5242880 bytes - 5 mb
+                if ($file->saveAs($uploadPath . '/' . $file->name)) {
+                    $pathFile = 'data/' . Yii::$app->user->id . '/' . $file->name;
+                    $this->setData(['photo' => $pathFile]);
+
+                    //Now save file data to database
+                    User::setUserPhoto($file->name);
+                }
+            } else {
+                $this->addError('photo', 'fileSize is too big. Max fileSize is 5 MiB.');
+            }
+        }
+
+    }
+
+}
