9d32f07a087b27f09a788a17560882ff58e24a0f
igor <igor.luchko@skynix.co>
2017-09-06 17:04:12 +0300

    SCA-19 add-acceptance-test-to-check-a-da

--- views/site/for_status.php
+++ views/site/for_status.php
@@ -9,7 +9,7 @@
 
 <div class="jumbotron">
     <h2>Skynix Services Status</h2>
-    <?php if (count($f > 0)) {
+    <?php if ($f) {
         echo '<h1 class="check_active_notice" style="color: #00aa00">Active</h2>';
     } else {
         echo '<h1 style="color: blue">Maintenance</h2>';
