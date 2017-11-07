a62c60ee907bafb1d1c4ead8e1f63d67c9ab2cda
igor <igor.luchko@skynix.co>
2017-11-07 12:31:26 +0200

    SCA-43-121-create-a-new-counterparty(add more test)

--- modules/api/tests/functional/CounterpartiesCest.php
+++ modules/api/tests/functional/CounterpartiesCest.php
@@ -60,4 +60,77 @@ class CounterpartiesCest
             "success" => false
         ]);
     }
+
+    /**
+     * @see    https://jira.skynix.company/browse/SCA-43
+     * @param  FunctionalTester $I
+     * @return void
+     */
+    public function testCounterpartyCreationForbiddenForSaleDevClientPm(FunctionalTester $I, \Codeception\Scenario $scenario)
+    {
+
+        $I->haveInDatabase('users', array(
+            'id' => 4,
+            'first_name' => 'saleUsers',
+            'last_name' => 'saleUsersLast',
+            'email' => 'saleUser@email.com',
+            'role' => 'SALES',
+            'password' => md5('sales')
+        ));
+
+        $I->haveInDatabase('users', array(
+            'id' => 5,
+            'first_name' => 'devUsers',
+            'last_name' => 'devUsersLast',
+            'email' => 'devUser@email.com',
+            'role' => 'DEV',
+            'password' => md5('dev')
+        ));
+
+        $I->haveInDatabase('users', array(
+            'id' => 6,
+            'first_name' => 'pmUsers',
+            'last_name' => 'pmUsersLast',
+            'email' => 'pmUser@email.com',
+            'role' => 'PM',
+            'password' => md5('pm')
+        ));
+
+        $I->haveInDatabase('users', array(
+            'id' => 7,
+            'first_name' => 'clientUsers',
+            'last_name' => 'clientUsersLast',
+            'email' => 'clientUser@email.com',
+            'role' => 'CLIENT',
+            'password' => md5('client')
+        ));
+
+
+        for ($i = 4; $i < 8; $i++) {
+            $email = $I->grabFromDatabase('users', 'email', array('id' => $i));
+            $pas = $I->grabFromDatabase('users', 'role', array('id' => $i));
+
+            \Helper\OAuthToken::$key = null;
+
+            $oAuth = new OAuthSteps($scenario);
+            $oAuth->login($email, strtolower($pas));
+
+        $I->wantTo('Test that counterparty creation forbidden for  DEV, SALES, PM, CLIENT role');
+        $I->sendPOST(ApiEndpoints::COUNTERPARTY );
+
+        \Helper\OAuthToken::$key = null;
+
+        $response = json_decode($I->grabResponse());
+        $I->assertNotEmpty($response->errors);
+        $I->seeResponseContainsJson([
+            "data" => null,
+            "errors" => [
+                "param" => "error",
+                "message" => "You have no permission for this action"
+            ],
+            "success" => false
+        ]);
+    }
+
+    }
 }
