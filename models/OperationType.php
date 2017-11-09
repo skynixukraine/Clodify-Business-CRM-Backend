1f9020abedee412becdf795ddd4578e3d91182eb
igor <igor.luchko@skynix.co>
2017-11-09 11:34:29 +0200

    SCA-55-531-create-a-new-operation

---
+++ models/OperationType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "operation_types".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property Operations[] $operations
+ */
+class OperationType extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'operation_types';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOperations()
+    {
+        return $this->hasMany(Operations::className(), ['operation_type_id' => 'id']);
+    }
+}
