e6a57ea9c9bb9e98f75150b981318e0533abb420
igor <igor.luchko@skynix.co>
2017-11-14 11:48:13 +0200

    SCA-56-532-update-an-existing-operation

--- modules/api/view-models/OperationUpdate.php
+++ modules/api/view-models/OperationUpdate.php
@@ -8,10 +8,13 @@
 
 namespace viewModel;
 
-use app\models\Counterparty;
+use app\models\Operation;
+use app\models\OperationType;
+use app\models\Transaction;
 use app\models\User;
 use app\modules\api\components\Api\Processor;
 use yii;
+use yii\db\Exception;
 
 class OperationUpdate extends ViewModelAbstract
 {
@@ -20,11 +23,82 @@ class OperationUpdate extends ViewModelAbstract
     {
         $id = Yii::$app->request->getQueryParam('id');
 
-        if (User::hasPermission([User::ROLE_ADMIN, User::ROLE_FIN])) {
+        if (User::hasPermission([User::ROLE_ADMIN])) {
+            $operation = Operation::findOne($id);
+
+            if ($operation) {
+                $transaction = Yii::$app->db->beginTransaction();
+                try {
+
+                    $name = $this->postData['name'];
+                    $operation_type_id = $this->postData['operation_type_id'];
+                    $transaction_name = $this->postData['transaction_name'];
+                    $amount = $this->postData['amount'];
+                    $currency = $this->postData['currency'];
+                    $status = $this->postData['status'];
+
+                    if (isset($name)) {
+                        $operation->name = $name;
+                    }
+                    if (isset($status)) {
+                        $operation->status = $status;
+                    }
+                    if (isset($operation_type_id)) {
+                        if(OperationType::validCount($operation_type_id)) {
+                            $operation->operation_type_id = $operation_type_id;
+                        } else {
+                            return $this->addError(Processor::ERROR_PARAM,
+                                Yii::t('yii', 'Your operation_type_id is out of allowed'));
+                        }
+                    }
+                    $operation->date_updated = time();
+                    $operation->setScenario(Operation::SCENARIO_OPERATION_UPDATE);
+
+                    // update transaction for DEBIT
+                    $transaction1 = Transaction::getDebitTransactionById($id);
+                    if (isset($transaction_name)) {
+                        $transaction1->name = $transaction_name;
+                    }
+                    if (isset($amount)) {
+                        $transaction1->amount = $amount;
+                    }
+                    if (isset($currency)) {
+                        $transaction1->currency = $currency;
+                    }
+
+                    // update transaction for CREDIT
+                    $transaction2 = Transaction::getCreditTransactionById($id);
+                    if (isset($transaction_name)) {
+                        $transaction2->name = $transaction_name;
+                    }
+                    if (isset($amount)) {
+                        $transaction2->amount = $amount;
+                    }
+                    if (isset($currency)) {
+                        $transaction2->currency = $currency;
+                    }
+
+                    if ($transaction1->save() && $transaction2->save() && $operation->save()) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollBack();
+                        return $this->addError(Processor::ERROR_PARAM,
+                            Yii::t('yii', 'Sorry, but the entered data is not correct and updating failed'));
+                    }
+
+                } catch (Exception $e) {
+                    $transaction->rollBack();
+                    throw $e;
+                }
+            } else {
+                return $this->addError(Processor::ERROR_PARAM,
+                    Yii::t('yii', 'You are trying to update data for not existent operation '));
+            }
 
         } else {
             return $this->addError(Processor::ERROR_PARAM,
                 Yii::t('yii', 'You have no permission for this action'));
         }
     }
+
 }
