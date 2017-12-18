ce4073297bafbb20f5f6f6fe5a0990a64ca6c17b
Igor Luchko <igor.luchko@skynix.co>
2017-12-18 17:56:17 +0200

    feature/SCA-62-implement-sso-for-skynix-crm-api

--- modules/api/view-models/Auth.php
+++ modules/api/view-models/Auth.php
@@ -11,6 +11,8 @@ use Yii;
 use app\modules\api\models\ApiAccessToken;
 use app\models\User;
 use app\modules\api\models\ApiLoginForm;
+use app\modules\api\components\Api\Processor;
+
 
 class Auth extends ViewModelAbstract
 {
@@ -28,14 +30,40 @@ class Auth extends ViewModelAbstract
             $loginForm->password    = $this->model->password;
             $this->model            = $loginForm;
 
-           if ($this->validate() && ($token = $this->model->login())) {
-               $user = User::findOne(['id' => $token->user_id]);
-               $this->setData([
-                   'access_token' => $token->access_token,
-                   'user_id' => $token->user_id,
-                   'role' => $user->role
-               ]);
-           }
+            $obj = $this->model->toCrowd();
+            if(!isset($obj->reason)){     // if element 'reason' exist, some autentication error there in crowd
+
+            $objToArray = json_decode(json_encode($obj), true);
+
+            if($obj->active){
+                $user = User::findOne(['email' => $loginForm->email]);
+                if(!empty($user)) {
+                    $sessionObj = ApiLoginForm::createCrowdSession($loginForm->email, $loginForm->password);
+                    $exp = ApiLoginForm::getExpireForSession($sessionObj);
+                } else{
+                    $newUser = new User();
+                    $newUser->role = User::ROLE_DEV;
+                    $newUser->first_name = $objToArray['first-name'];
+                    $newUser->last_name = $objToArray['last-name'];
+                    $newUser->email = $obj->email;
+                    $newUser->password = $loginForm->password;
+                    $newUser->save();
+                    $sessionObj = ApiLoginForm::createCrowdSession($newUser->email, $newUser->password);
+                    $exp = ApiLoginForm::getExpireForSession($sessionObj);
+                }
+                $this->setData([
+                    'expand' => $sessionObj->expand,
+                    'token' => $sessionObj->token,
+                    'expiry-date' => $exp,
+                ]);
+
+            } else {
+                return $this->addError(Processor::ERROR_PARAM, Yii::t('yii', 'Your account is suspended, contact Skynix administrator'));
+            }
+        } else {
+            return $this->addError(Processor::ERROR_PARAM, Yii::t('yii',  $obj->reason . ' Your username or password is wrong. Use your skynix credentials'));
+        }
+
         }
     }
 
