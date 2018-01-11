cc2413d6c878a6e92f17405ef8273b52d0164b1a
Igor Luchko <igor.luchko@skynix.co>
2018-01-11 13:22:19 +0200

    feature/SCA-69-2.6.3-fetch-financial-reports-response-to-rest-standards-change-url

--- modules/api/tests/_support/Helper/ApiEndpoints.php
+++ modules/api/tests/_support/Helper/ApiEndpoints.php
@@ -10,26 +10,27 @@ namespace Helper;
 
 class ApiEndpoints
 {
-    const CONTACT           = '/api/contacts';
-    const CONTACT_ATTACH    = self::CONTACT . '/attachment';
-    const PASSWORD          = '/api/password';
-    const REPORT            = '/api/reports';
-    const USERS             = '/api/users';
-    const PROJECT           = '/api/projects';
-    const ATTACH_SIGN       = '/api/users/sign';
-    const ATTACH_PHOTO      = '/api/users/photo';
-    const CAREERS_VIEW      = '/api/careers';
-    const SURVEY            = '/api/surveys';
-    const PROFILES          = '/api/profiles';
-    const CONTRACTS         = '/api/contracts';
-    const USER              = '/api/user';
-    const FINANCIAL_REPORTS = '/api/financial-reports';
-    const SALARY_REPORTS    = '/api/salary-reports';
-    const SETTINGS          = '/api/settings';
-    const COUNTERPARTY      = '/api/counterparties';
-    const OPERATION         = '/api/operations';
-    const BUSINESS          = '/api/businesses';
-    const OPERATION_TYPES   = '/api/operation-types';
-    const REFERENCE_BOOK    = '/api/reference-book-items';
+    const CONTACT                 = '/api/contacts';
+    const CONTACT_ATTACH          = self::CONTACT . '/attachment';
+    const PASSWORD                = '/api/password';
+    const REPORT                  = '/api/reports';
+    const USERS                   = '/api/users';
+    const PROJECT                 = '/api/projects';
+    const ATTACH_SIGN             = '/api/users/sign';
+    const ATTACH_PHOTO            = '/api/users/photo';
+    const CAREERS_VIEW            = '/api/careers';
+    const SURVEY                  = '/api/surveys';
+    const PROFILES                = '/api/profiles';
+    const CONTRACTS               = '/api/contracts';
+    const USER                    = '/api/user';
+    const FINANCIAL_REPORTS       = '/api/financial-reports';
+    const SALARY_REPORTS          = '/api/salary-reports';
+    const SETTINGS                = '/api/settings';
+    const COUNTERPARTY            = '/api/counterparties';
+    const OPERATION               = '/api/operations';
+    const BUSINESS                = '/api/businesses';
+    const OPERATION_TYPES         = '/api/operation-types';
+    const REFERENCE_BOOK          = '/api/reference-book-items';
+    const FETCH_FINANCIAL_REPORTS = '/api/financialReports';
 
 }
