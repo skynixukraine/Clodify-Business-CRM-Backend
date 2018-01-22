55047ad870d94eef3c119eea110195e7aae145c2
Igor Luchko <igor.luchko@skynix.co>
2018-01-22 17:46:56 +0200

    feature/SCA-75-migrate-existing-user-from-mysql-to-crowd-authorization(add removed migration)

--- migrations/m180118_094629_add_column_authtype_to_users.php
+++ migrations/m180118_094629_add_column_authtype_to_users.php
@@ -7,10 +7,27 @@ class m180118_094629_add_column_authtype_to_users extends Migration
     public function up()
     {
         $this->addColumn( 'users', 'auth_type', 'tinyint(1) DEFAULT 1');
+
+        // add foreign key for table `users`
+        $this->addForeignKey(
+            'fk_users_auth_type',
+            'users',
+            'auth_type',
+            'auth_types',
+            'id',
+            'NO ACTION',
+            'NO ACTION'
+        );
     }
 
     public function down()
     {
+        // drops foreign key for table users
+        $this->dropForeignKey(
+            'fk_users_auth_type',
+            'users'
+        );
+
         $this->dropColumn('users', 'auth_type');
     }
 }
