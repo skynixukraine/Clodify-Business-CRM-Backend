53d18811cfadd98d4a00d81b602069a3a6606648
Igor Luchko <igor.luchko@skynix.co>
2018-01-22 17:07:33 +0200

    feature/SCA-75-migrate-existing-user-from-mysql-to-crowd-authorization(setdata in auth)

--- modules/api/view-models/Auth.php
+++ modules/api/view-models/Auth.php
@@ -18,45 +18,43 @@ class Auth extends ViewModelAbstract
 
     /** @var  \app\models\User */
     public $model;
+    public $token;
 
     public function define()
     {
         $this->model->scenario = 'api-login';
 
-        if($this->validate()) {
+        if ($this->validate()) {
             $loginForm = new ApiLoginForm();
-            $loginForm->email       = $this->model->email;
-            $loginForm->password    = $this->model->password;
-            $this->model            = $loginForm;
-
-            // crowd session code go here
-            $checkUser = Yii::$app->crowdComponent->checkByEmailPassword($loginForm->email, $loginForm->password);
-            if(isset($checkUser['error'])){
-                $this->addError(Processor::CROWD_ERROR_PARAM, Yii::t('yii', $checkUser['error']));
-            }
-
+            $loginForm->email = $this->model->email;
+            $loginForm->password = $this->model->password;
+            $this->model = $loginForm;
 
             $user = $this->model->getUser();
-            if($user->auth_type == User::CROWD_AUTH){
-                if(!is_array($checkUser)) {
-                    $token = $this->model->login();
-                    $this->setData([
-                        'access_token' => $token->access_token,
-                        'user_id' => $token->user_id,
-                        'role' => $user->role
-                    ]);
+            if ($user->auth_type == User::CROWD_AUTH) {
+
+                $checkUser = Yii::$app->crowdComponent->checkByEmailPassword($loginForm->email, $loginForm->password);
+                if (isset($checkUser['error'])) {
+                    $this->addError(Processor::CROWD_ERROR_PARAM, Yii::t('yii', $checkUser['error']));
+                }
+
+                if (!is_array($checkUser)) {
+                    $this->token = $this->model->login();
                 }
             }
 
-            if($user->auth_type == User::DATABASE_AUTH){
-                if($this->validate()){
-                    $token = $this->model->login();
-                    $this->setData([
-                        'access_token' => $token->access_token,
-                        'user_id' => $token->user_id,
-                        'role' => $user->role
-                    ]);
+            if ($user->auth_type == User::DATABASE_AUTH) {
+                if ($this->validate()) {
+                    $this->token = $this->model->login();
                 }
+            }
+
+            if (isset($this->token)) {
+                $this->setData([
+                    'access_token' => $this->token->access_token,
+                    'user_id' => $this->token->user_id,
+                    'role' => $user->role
+                ]);
             }
         }
     }
