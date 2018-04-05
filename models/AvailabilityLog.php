b0bfac098e6ecbdb8bd18a6bdc549fcf11a232b6
Igor Luchko <igor.luchko@skynix.co>
2018-04-05 18:11:28 +0300

    SCA-125 (without test early one)

---
+++ models/AvailabilityLog.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use app\models\User;
+
+/**
+ * This is the model class for table "availability_logs".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $date
+ * @property int $is_available
+ *
+ * @property User $user
+ */
+class AvailabilityLog extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'availability_logs';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'date'], 'integer'],
+            [['is_available'], 'string', 'max' => 1],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'date' => 'Date',
+            'is_available' => 'Is Available',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
---
+++ modules/api/view-models/ResourceFetch.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by Skynix Team.
+ * User: igor
+ * Date: 5.04.18
+ * Time: 16:33
+ */
+
+namespace viewModel;
+
+use app\models\ReferenceBook;
+use app\models\Transaction;
+use Yii;
+use app\modules\api\components\SortHelper;
+use app\components\DataTable;
+use app\models\User;
+use app\modules\api\components\Api\Processor;
+
+/**
+ * Class ResourceFetch
+ * @see     https://jira.skynix.co/browse/SCA-125
+ * @package viewModel
+ */
+class ResourceFetch extends ViewModelAbstract
+{
+    public function define()
+    {
+
+        if (User::hasPermission([User::ROLE_ADMIN, User::ROLE_FIN,])) {
+            $order = Yii::$app->request->getQueryParam('order');
+            $start = Yii::$app->request->getQueryParam('start') ?: 0;
+            $limit = Yii::$app->request->getQueryParam('limit') ?: SortHelper::DEFAULT_LIMIT;
+
+            $query = User::find()->where(['role'=> [User::ROLE_DEV, User::ROLE_SALES, User::ROLE_ADMIN, User::ROLE_PM]])->with('availabilityLog');
+
+            $dataTable = DataTable::getInstance()
+                ->setQuery($query)
+                ->setLimit($limit)
+                ->setStart($start);
+
+            if ($order) {
+                foreach ($order as $name => $value) {
+                    $dataTable->setOrder(User::tableName() . '.' . $name, $value);
+                }
+
+            } else {
+                $dataTable->setOrder(User::tableName() . '.id', 'desc');
+            }
+
+            $resources = $dataTable->getData();
+
+            if ($resources) {
+                foreach ($resources as $k => $v) {
+                    $resources[$k] = [
+                        "id" => $v->id,
+                        "first_name" => $v->first_name,
+                        "last_name" => $v->last_name,
+                        "is_available" =>  $v->is_available ? true : false,
+                        "available_for" => $v->is_available ? User::getAvailabilityTime($v->id) : null
+                    ];
+                }
+            } else {
+                $resources = [];
+            }
+            $data = [
+                'resources' => $resources,
+                'total_records' => DataTable::getInstance()->getTotal()
+            ];
+
+            $this->setData($data);
+
+        } else {
+            return $this->addError(Processor::ERROR_PARAM, Yii::t('yii', 'You have no permission for this action'));
+        }
+
+    }
+}
