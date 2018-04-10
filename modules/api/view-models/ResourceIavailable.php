d331885a0f3eb230867048b5c39f77f449e670d8
Igor Luchko <igor.luchko@skynix.co>
2018-04-10 11:56:14 +0300

    SCA-126

--- modules/api/view-models/ResourceIavailable.php
+++ modules/api/view-models/ResourceIavailable.php
@@ -30,13 +30,26 @@ class ResourceIavailable extends ViewModelAbstract
 
             $userId = Yii::$app->user->id;
 
-            User::updateAll(['is_available' => 1], ['id' => $userId]);
+            if (!User::findOne($userId)->is_available) {
 
-            $availabilityLog = new AvailabilityLog();
-            $availabilityLog->user_id = $userId;
-            $availabilityLog->date = time();
-            $availabilityLog->is_available = 1;
-            $availabilityLog->save();
+                $log = AvailabilityLog::find()->where(['user_id' => $userId])->one();
+
+                // if log for that user not exist
+                if (!$log){
+                    // create log
+                    $availabilityLog = new AvailabilityLog();
+                    $availabilityLog->user_id = $userId;
+                    $availabilityLog->date = time();
+                    $availabilityLog->is_available = 1;
+                    $availabilityLog->save();
+                    // update user table
+                    User::updateAll(['is_available' => 1], ['id' => $userId]);
+                    //  if log exist: update logs and users tables
+                } else {
+                    AvailabilityLog::updateAll(['is_available' => 1, 'date' => time()], ['user_id' => $userId]);
+                    User::updateAll(['is_available' => 1], ['id' => $userId]);
+                }
+            }
 
         } else {
             return $this->addError(Processor::ERROR_PARAM, Yii::t('yii', 'You have no permission for this action'));
