9c7c69c9bbaf203b3ecabf3990a05e1840659611
Igor Luchko <igor.luchko@skynix.co>
2018-04-18 14:50:07 +0300

    SCA-143(first commit add migration)

---
+++ migrations/m180418_111016_add_is_deleted_col_to_operations_tbl.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180418_111016_add_is_deleted_col_to_operations_tbl
+ */
+class m180418_111016_add_is_deleted_col_to_operations_tbl extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn( 'operations', 'is_deleted', $this->tinyInteger()->defaultValue(0));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180418_111016_add_is_deleted_col_to_operations_tbl cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180418_111016_add_is_deleted_col_to_operations_tbl cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
