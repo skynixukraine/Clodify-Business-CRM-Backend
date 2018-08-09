e27bd74349c36c725edc0f1e7a8de584cbca1d82
oprystashkin <super@gmail.com>
2018-08-09 15:50:36 +0300

    SCA-226 add test a payment method creation is unable on missing a required field

--- modules/api/tests/functional/PaymentMethodsCest.php
+++ modules/api/tests/functional/PaymentMethodsCest.php
@@ -12,11 +12,21 @@ class PaymentMethodsCest
 {
 
     /**
-     * @see    https://jira-v2.skynix.company/browse/SI-946
+     * @see    https://jira-v2.skynix.company/browse/SCA-226
      * @param FunctionalTester $I
      */
     public function testCreatePaymentMethods(FunctionalTester $I, \Codeception\Scenario $scenario)
     {
+
+
+        $paymentMethodData = [
+            'name' => 'p24',
+            'address' => 'Kyiv 22, ap 33',
+            'represented_by' => 'Privat 24',
+            'bank_information' => 'The P24 is a large bank in Ukraine',
+            'is_default' => 0,
+            'business_id' => 1
+        ];
 
         $I->wantTo('test payment method creation is forbidden for DEV, PM, CLIENT role');
         $email = $I->grabFromDatabase('users', 'email', array('id' => ValuesContainer::$userDev['id']));
@@ -28,14 +38,7 @@ class PaymentMethodsCest
         $oAuth->login($email, $pas);
 
         $I->wantTo('test payment method creation is forbidden for DEV role');
-        $I->sendPOST('/api/businesses/1/methods', json_encode([
-            'name' => 'p24',
-            'address' => 'Kyiv 22, ap 33',
-            'represented_by' => 'Privat 24',
-            'bank_information' => 'The P24 is a large bank in Ukraine',
-            'is_default' => 0,
-            'business_id' => 1
-        ]));
+        $I->sendPOST('/api/businesses/1/methods', json_encode($paymentMethodData));
 
         \Helper\OAuthToken::$key = null;
 
@@ -57,14 +60,7 @@ class PaymentMethodsCest
         $oAuth = new OAuthSteps($scenario);
         $oAuth->login($email, $pas);
 
-        $I->sendPOST('/api/businesses/1/methods', json_encode([
-            'name' => 'p24',
-            'address' => 'Kyiv 22, ap 33',
-            'represented_by' => 'Privat 24',
-            'bank_information' => 'The P24 is a large bank in Ukraine',
-            'is_default' => 0,
-            'business_id' => 1
-        ]));
+        $I->sendPOST('/api/businesses/1/methods', json_encode($paymentMethodData));
 
         \Helper\OAuthToken::$key = null;
 
@@ -88,14 +84,7 @@ class PaymentMethodsCest
         $oAuth = new OAuthSteps($scenario);
         $oAuth->login($email, $pas);
 
-        $I->sendPOST('/api/businesses/1/methods', json_encode([
-            'name' => 'p24',
-            'address' => 'Kyiv 22, ap 33',
-            'represented_by' => 'Privat 24',
-            'bank_information' => 'The P24 is a large bank in Ukraine',
-            'is_default' => 0,
-            'business_id' => 1
-        ]));
+        $I->sendPOST('/api/businesses/1/methods', json_encode($paymentMethodData));
 
         \Helper\OAuthToken::$key = null;
 
@@ -120,14 +109,7 @@ class PaymentMethodsCest
         $oAuth = new OAuthSteps($scenario);
         $oAuth->login($email, $pas);
 
-        $I->sendPOST('/api/businesses/1/methods', json_encode([
-            'name' => 'p24',
-            'address' => 'Kyiv 22, ap 33',
-            'represented_by' => 'Privat 24',
-            'bank_information' => 'The P24 is a large bank in Ukraine',
-            'is_default' => 0,
-            'business_id' => 1
-        ]));
+        $I->sendPOST('/api/businesses/1/methods', json_encode($paymentMethodData));
 
         \Helper\OAuthToken::$key = null;
 
@@ -151,14 +133,7 @@ class PaymentMethodsCest
         $oAuth = new OAuthSteps($scenario);
         $oAuth->login($email, $pas);
 
-        $I->sendPOST('/api/businesses/1/methods', json_encode([
-            'name' => 'p24',
-            'address' => 'Kyiv 22, ap 33',
-            'represented_by' => 'Privat 24',
-            'bank_information' => 'The P24 is a large bank in Ukraine',
-            'is_default' => 0,
-            'business_id' => 1
-        ]));
+        $I->sendPOST('/api/businesses/1/methods', json_encode($paymentMethodData));
 
         \Helper\OAuthToken::$key = null;
 
@@ -179,8 +154,52 @@ class PaymentMethodsCest
 
     }
 
+    public function testCreatePaymentMethodsOnRequiredFieldMissing(FunctionalTester $I, \Codeception\Scenario $scenario){
+        $paymentMethodData = [
+            'name' => 'p24',
+            'address' => 'Kyiv 22, ap 33',
+            'represented_by' => 'Privat 24',
+            'bank_information' => 'The P24 is a large bank in Ukraine',
+            'is_default' => 0,
+            'business_id' => 1
+        ];
+
+        $I->wantTo('test a payment method creation is unable on missing a required field');
+
+        $email = $I->grabFromDatabase('users', 'email', array('id' => ValuesContainer::$userAdmin['id']));
+        $pas = ValuesContainer::$userAdmin['password'];
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login($email, $pas);
+
+        foreach($paymentMethodData as $key => $elem) {
+
+            $testData = $paymentMethodData;
+            unset($testData[$key]);
+
+            $I->sendPOST('/api/businesses/1/methods', json_encode($testData));
+
+            \Helper\OAuthToken::$key = null;
+
+            $I->seeResponseCodeIs('200');
+            $I->seeResponseIsJson();
+
+            $response = json_decode($I->grabResponse());
+            $I->assertNotEmpty($response->errors);
+
+            $I->seeResponseMatchesJsonType([
+                'data' => "null",
+                'errors' => [[
+                    "param" => "string",
+                    "message" => "string"
+                ]],
+                'success' => 'boolean'
+            ]);
+
+        }
+    }
+
     /**
-     * @see    https://jira-v2.skynix.company/browse/SI-946
+     * @see    https://jira-v2.skynix.company/browse/SCA-230
      * @param FunctionalTester $I
      */
     public function testFetchPaymentMethods(FunctionalTester $I, \Codeception\Scenario $scenario)
