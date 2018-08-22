115843c8837ca0ddd31ebe607e55a92a4559d03d
oleg.master.developer@gmail.com <oleg.master.developer@gmail.com>
2018-08-22 10:45:49 +0300

    SCA-237 add subscribe a project API, tests

---
+++ modules/api/view-models/ProjectSubscribe.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by SkynixTeam.
+ * User: Oleg
+ * Date: 22.08.18
+ * Time: 16:01
+ */
+
+namespace viewModel;
+use Yii;
+use app\models\ProjectDeveloper;
+use app\modules\api\components\Api\Processor;
+
+class ProjectSubscribe extends ViewModelAbstract
+{
+    public function define(){
+
+            $projectDeveloperId = Yii::$app->request->getQueryParam('id');
+            $projectDeveloper = ProjectDeveloper::findOne(['project_id' => $projectDeveloperId]);
+
+            if(is_null($projectDeveloper)){
+                return $this->addError(Processor::ERROR_PARAM, Yii::t('app','project is\'t found by Id'));
+            }
+
+
+            $projectDeveloper->status = 'ACTIVE';
+
+            if($this->validate()) {
+                $projectDeveloper->save();
+            }
+
+    }
+}
