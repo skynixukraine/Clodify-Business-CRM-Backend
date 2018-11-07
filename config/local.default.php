bfc834a38959f29b75ffd4aab326c0af2b2c1653
Anton Matyunin <anton.matyunin@skynix.co>
2018-11-07 15:57:44 +0200

    local.default.php edited online with Bitbucket

--- config/local.default.php
+++ config/local.default.php
@@ -13,9 +13,9 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=in.skynix',
-            'username' => 'mysql_user',
-            'password' => 'mysql_pass'
+            'dsn' => 'mysql:host=<mysql_host_name>;dbname=<mysql_db_name>',
+            'username' => '<mysql_user>',
+            'password' => '<mysql_pass>'
         ],
         'dbCore' => [
             'dsn' => 'mysql:host=<mysql_host_name>;dbname=<core_db_name>',
