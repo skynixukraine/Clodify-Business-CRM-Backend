c015975b20d9c8d5149ec75ec510f591a029fcba
oprystashkin <super@gmail.com>
2018-11-09 16:37:04 +0200

    correct route

--- config/route.php
+++ config/route.php
@@ -114,7 +114,7 @@ return [
     'GET '      . $API . '/email-templates'                                   => $API . '/email-templates/fetch',
     'GET '      . $API . '/email-templates/<id:\d+>'                          => $API . '/email-templates/fetch',
     'PUT '      . $API . '/email-templates/<id:\d+>'                          => $API . '/email-templates/update',
-    'POST '     . $API . '/login-as-user/<user_id:\d+>'                      => $API . '/users/login-as-user',
+    'POST '     . $API . '/login-as-user/<user_id:\d+>'                       => $API . '/users/login-as-user',
 
 
 
