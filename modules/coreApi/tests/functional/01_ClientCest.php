6f400dbf62644492ed7a243d76df400ad043721d
oprystashkin <super@gmail.com>
2018-11-09 16:26:07 +0200

    add fetch client test

--- modules/coreApi/tests/functional/01_ClientCest.php
+++ modules/coreApi/tests/functional/01_ClientCest.php
@@ -31,4 +31,70 @@ class ClientCest
         $I->assertEquals(true, $response->success);
 
     }
+
+    /**
+     * @see    https://jira-v2.skynix.company/browse/SCA-230
+     * @param FunctionalTester $I
+     */
+    public function fetchClientTest(FunctionalTester $I, \Codeception\Scenario $scenario)
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login();
+
+        $I->wantTo('Testing fetch counterparties data');
+        $I->sendGET(ApiEndpoints::BUSINESS);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse());
+        $I->assertEmpty($response->errors);
+        $I->assertEquals(true, $response->success);
+        $I->seeResponseMatchesJsonType([
+            'data' => [[
+                'id' => 'integer',
+                'name' => 'string',
+                'address' => 'string',
+                'is_default' => 'integer|null',
+                'director' => [
+                    'id' => 'integer',
+                    'first_name' => 'string',
+                    'last_name' => 'string'
+                ]
+
+            ]],
+            'errors' => [],
+            'success' => 'boolean'
+        ]);
+
+    }
+
+    /**
+     * @see https://jira.skynix.co/browse/SCA-232
+     * @param FunctionalTester $I
+     * @param \Codeception\Scenario $scenario
+     * @return void
+     */
+    public function fetchClientForbiddenNotAuthorizedTest(FunctionalTester $I)
+    {
+
+        \Helper\OAuthToken::$key = null;
+
+        $I->wantTo('test business create is forbidden for not authorized');
+        $I->sendGET(ApiEndpoints::BUSINESS);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse());
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals(false, $response->success);
+        $I->seeResponseContainsJson([
+            "data" => null,
+            "errors" => [
+                "param" => "error",
+                "message" => "You are not authorized to access this action"
+            ],
+            "success" => false
+        ]);
+
+
+    }
 }
+
