9857b49df6fb383c8c46a342bd629a0a298fd419
Oleksii Prozhoha <admin@skynix.co>
2018-11-13 16:37:36 +0200

    SCA-281 updated test form

--- views/site/testorder.php
+++ views/site/testorder.php
@@ -10,8 +10,9 @@
     <input type="hidden" name="amt" value="0.5"/>
     <input type="hidden" name="ccy" value="UAH"/>
     <input type="hidden" name="merchant" value="140273"/>
-    <input type="hidden" name="order" value="042343"/>
-    <input type="hidden" name="details" value="Services for November 2018"/>
+    <input type="hidden" name="order" value="1"/>
+    <input type="hidden" name="details" value="Order #1"/>
+    <input type="hidden" name="ext_details" value="Services for November 2018"/>
     <input type="hidden" name="return_url" value="https://staging.core.api.skynix.co/core-api/payments/status"/>
     <input type="hidden" name="server_url" value="https://staging.core.api.skynix.co/core-api/payments"/>
     <input type="hidden" name="pay_way" value="privat24"/>
