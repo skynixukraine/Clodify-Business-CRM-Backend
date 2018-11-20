db217daeea3bac4ecd982d2159a0d9439f85703b
oprystashkin <super@gmail.com>
2018-11-20 17:52:32 +0200

    add email send on client create

--- modules/coreApi/view-models/ClientRegistration.php
+++ modules/coreApi/view-models/ClientRegistration.php
@@ -13,6 +13,7 @@ use Yii;
 use app\models\User;
 use app\modules\api\models\ApiLoginForm;
 use app\modules\api\components\Api\Processor;
+use app\models\EmailTemplate;
 
 
 class ClientRegistration extends ViewModelAbstract
@@ -34,6 +35,25 @@ class ClientRegistration extends ViewModelAbstract
             $this->model->mysql_password  = Yii::$app->security->generateRandomString( 12 );
 
             if ( $this->validate() && $this->model->save() ) {
+
+                $emailTemplate = EmailTemplate::findOne(['subject' => 'Skynix CRM configuration']);
+
+
+                $mail = Yii::$app->mailer->compose($emailTemplate, [
+                    'username' => $this->model->first_name,
+                    'link_to_crm' => '',
+                    'domain' => $this->model->domain,
+                    'email' => $this->model->email,
+                    'auto_generated_password' => $this->model->mysql_password,
+
+                ])
+                    ->setFrom(Yii::$app->params['adminEmail'])
+                    ->setTo($this->model->email)
+                    ->setSubject('Skynix CRM: client registration');
+
+                if (!$mail->send()) {
+                    $this->addError('email', "Skynix CRM configuration");
+                }
 
                 $this->setData([
                     'client_id' => $this->model->id
