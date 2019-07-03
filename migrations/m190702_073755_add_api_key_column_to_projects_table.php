a592dcd7d06b29bddf285f3fca3a99306ffbc46a
Oleg Zalozhenkov <oleg.zalozhenkov@skynix.co>
2019-07-03 14:32:13 +0300

    create migration to add api_key column to the projects table

---
+++ migrations/m190702_073755_add_api_key_column_to_projects_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use app\components\SkynixMigration;
+
+/**
+ * Handles adding api_key to table `projects`.
+ */
+class m190702_073755_add_api_key_column_to_projects_table extends SkynixMigration
+{
+    public $isCore = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->addColumn('projects', 'api_key', $this->string(32));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->dropColumn('projects', 'api_key');
+    }
+}
