17c646ce5c3a848560cea4d24c9b952e82b97ad2
Oleg Zalozhenkov <oleg.zalozhenkov@skynix.co>
2019-07-10 15:43:21 +0300

    add route to delete env variables

--- config/route.php
+++ config/route.php
@@ -18,6 +18,7 @@ return [
     'DELETE '   . $API . '/projects/<id:\d+>/subscription'                    => $API . '/projects/unsubscribe',
     'POST '     . $API . '/projects/<id:\d+>/env/<branch_name:\w+>'           => $API . '/project-environment-variables/create',
     'GET '      . $API . '/projects/<id:\d+>/env/<branch_name:\w+>'           => $API . '/project-environment-variables/fetch',
+    'DELETE '   . $API . '/projects/<id:\d+>/env/<branch_name:\w+>/<var_id:\d+>' => $API . '/project-environment-variables/delete',
     'DELETE '   . $API . '/reports/<id:\d+>'                                  => $API . '/reports/delete',
     'PUT '      . $API . '/reports/<id:\d+>'                                  => $API . '/reports/create-edit',
     'POST '     . $API . '/reports'                                           => $API . '/reports/create-edit',
