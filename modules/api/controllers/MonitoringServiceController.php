e50f636bd656f12eacc8d30d997709cf9d7da501
Oleg Zalozhenkov <oleg.zalozhenkov@skynix.co>
2019-07-15 17:04:39 +0300

    add action to delete monitoring services

--- modules/api/controllers/MonitoringServiceController.php
+++ modules/api/controllers/MonitoringServiceController.php
@@ -10,6 +10,7 @@ use app\modules\api\components\Api\Processor;
 use viewModel\MonitoringServiceCreate;
 use viewModel\MonitoringServiceUpdate;
 use viewModel\MonitoringServiceFetch;
+use viewModel\MonitoringServiceDelete;
 use viewModel\ViewModelInterface;
 use yii\db\ActiveRecordInterface;
 
@@ -48,6 +49,19 @@ class MonitoringServiceController extends DefaultController
             ->set(ViewModelInterface::class, MonitoringServiceUpdate::class)
             ->set(Access::class, [
                 'methods' => [Processor::METHOD_PUT],
+                'checkAccess' => true,
+            ])
+            ->get('Processor')
+            ->respond();
+    }
+
+    public function actionDelete()
+    {
+        $this->di
+            ->set(ActiveRecordInterface::class, MonitoringService::class)
+            ->set(ViewModelInterface::class, MonitoringServiceDelete::class)
+            ->set(Access::class, [
+                'methods' => [Processor::METHOD_DELETE],
                 'checkAccess' => true,
             ])
             ->get('Processor')
