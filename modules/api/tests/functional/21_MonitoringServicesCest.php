8fbc7038855342f8726e6faa16d36e42e2fde87d
Oleg Zalozhenkov <oleg.zalozhenkov@skynix.co>
2019-07-15 12:06:09 +0300

    test monitoring service updating

--- modules/api/tests/functional/21_MonitoringServicesCest.php
+++ modules/api/tests/functional/21_MonitoringServicesCest.php
@@ -9,36 +9,36 @@ use Helper\ValuesContainer;
 
 class MonitoringServicesCest
 {
-    public function createServiceFailedForRolesExceptAdmin(FunctionalTester $I, Scenario $scenario): void
+    private function testFailCreateServiceForUser(FunctionalTester $I, Scenario $scenario, array $user)
     {
-        function testCreateService(FunctionalTester $I, Scenario $scenario, array $user)
-        {
-            $oAuth = new OAuthSteps($scenario);
-            $oAuth->login($user['email'], $user['password']);
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login($user['email'], $user['password']);
 
-            $I->sendPOST(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services',
-                json_encode([
-                    'url' => 'http://google.com',
-                    'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
-                ]));
-            $I->seeResponseCodeIs(200);
-            $I->seeResponseIsJson();
-            $response = json_decode($I->grabResponse(), false);
-            $I->assertNotEmpty($response->errors);
-            $I->assertEquals($response->success, false);
-            $I->assertEquals($response->data, null);
-            $I->cantSeeInDatabase('monitoring_services', [
+        $I->sendPOST(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services',
+            json_encode([
                 'url' => 'http://google.com',
                 'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
-                'project_id' => ValuesContainer::$projectWithEnvId,
-            ]);
-        }
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+        $I->cantSeeInDatabase('monitoring_services', [
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => ValuesContainer::$projectWithEnvId,
+        ]);
+    }
 
-        testCreateService($I, $scenario, ValuesContainer::$userClient);
-        testCreateService($I, $scenario, ValuesContainer::$userSales);
-        testCreateService($I, $scenario, ValuesContainer::$userDev);
-        testCreateService($I, $scenario, ValuesContainer::$userFin);
-        testCreateService($I, $scenario, ValuesContainer::$userPm);
+    public function createServiceFailedForRolesExceptAdmin(FunctionalTester $I, Scenario $scenario): void
+    {
+        $this->testFailCreateServiceForUser($I, $scenario, ValuesContainer::$userClient);
+        $this->testFailCreateServiceForUser($I, $scenario, ValuesContainer::$userSales);
+        $this->testFailCreateServiceForUser($I, $scenario, ValuesContainer::$userDev);
+        $this->testFailCreateServiceForUser($I, $scenario, ValuesContainer::$userFin);
+        $this->testFailCreateServiceForUser($I, $scenario, ValuesContainer::$userPm);
     }
 
     public function createServiceFailedIfWrongParams(FunctionalTester $I, Scenario $scenario): void
@@ -165,26 +165,168 @@ class MonitoringServicesCest
         $I->assertEquals($response->data, null);
     }
 
+    private function testFailGetServiceForUser(FunctionalTester $I, Scenario $scenario, array $user): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login($user['email'], $user['password']);
+
+        $I->sendGET(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+    }
+
     public function getServiceFailedForRolesExceptAdmin(FunctionalTester $I, Scenario $scenario): void
     {
-        function testGetService(FunctionalTester $I, Scenario $scenario, array $user)
-        {
-            $oAuth = new OAuthSteps($scenario);
-            $oAuth->login($user['email'], $user['password']);
+        $this->testFailGetServiceForUser($I, $scenario, ValuesContainer::$userClient);
+        $this->testFailGetServiceForUser($I, $scenario, ValuesContainer::$userSales);
+        $this->testFailGetServiceForUser($I, $scenario, ValuesContainer::$userDev);
+        $this->testFailGetServiceForUser($I, $scenario, ValuesContainer::$userFin);
+        $this->testFailGetServiceForUser($I, $scenario, ValuesContainer::$userPm);
+    }
 
-            $I->sendGET(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services');
-            $I->seeResponseCodeIs(200);
-            $I->seeResponseIsJson();
-            $response = json_decode($I->grabResponse(), false);
-            $I->assertNotEmpty($response->errors);
-            $I->assertEquals($response->success, false);
-            $I->assertEquals($response->data, null);
-        }
+    private function testFailUpdateServiceForUser(FunctionalTester $I, Scenario $scenario, array $user): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login($user['email'], $user['password']);
 
-        testGetService($I, $scenario, ValuesContainer::$userClient);
-        testGetService($I, $scenario, ValuesContainer::$userSales);
-        testGetService($I, $scenario, ValuesContainer::$userDev);
-        testGetService($I, $scenario, ValuesContainer::$userFin);
-        testGetService($I, $scenario, ValuesContainer::$userPm);
+        $I->sendPUT(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services/1',
+            json_encode([
+                'url' => 'http://google.com',
+                'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+                'is_enabled' => 0,
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+        $I->cantSeeInDatabase('monitoring_services', [
+            'id' => 1,
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => ValuesContainer::$projectWithEnvId,
+            'is_enabled' => 0,
+        ]);
+    }
+
+    public function updateServiceFailedForRolesExceptAdmin(FunctionalTester $I, Scenario $scenario): void
+    {
+        $this->testFailUpdateServiceForUser($I, $scenario, ValuesContainer::$userClient);
+        $this->testFailUpdateServiceForUser($I, $scenario, ValuesContainer::$userSales);
+        $this->testFailUpdateServiceForUser($I, $scenario, ValuesContainer::$userDev);
+        $this->testFailUpdateServiceForUser($I, $scenario, ValuesContainer::$userFin);
+        $this->testFailUpdateServiceForUser($I, $scenario, ValuesContainer::$userPm);
+    }
+
+    public function updateServiceFailedIfWrongParams(FunctionalTester $I, Scenario $scenario): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login(ValuesContainer::$userAdmin['email'], ValuesContainer::$userAdmin['password']);
+
+        $I->sendPUT(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services/1',
+            json_encode([
+                'url2' => 'http://google.com',
+                'notification_emails123' => 'test@gmail.com, qwerty@gmail.com',
+                'is_enabled5' => 0,
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+
+        $I->cantSeeInDatabase('monitoring_services', [
+            'id' => 1,
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => ValuesContainer::$projectWithEnvId,
+            'is_enabled' => 0,
+        ]);
+    }
+
+    public function updateServiceFailedIfWrongProjectId(FunctionalTester $I, Scenario $scenario): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login(ValuesContainer::$userAdmin['email'], ValuesContainer::$userAdmin['password']);
+
+        $I->sendPUT(ApiEndpoints::PROJECT . '/555/monitoring-services/1',
+            json_encode([
+                'url' => 'http://google.com',
+                'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+                'is_enabled' => 0,
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+
+        $I->cantSeeInDatabase('monitoring_services', [
+            'id' => 1,
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => 555,
+            'is_enabled' => 0,
+        ]);
+    }
+
+    public function updateServiceFailedIfWrongServiceId(FunctionalTester $I, Scenario $scenario): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login(ValuesContainer::$userAdmin['email'], ValuesContainer::$userAdmin['password']);
+
+        $I->sendPUT(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services/555',
+            json_encode([
+                'url' => 'http://google.com',
+                'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+                'is_enabled' => 0,
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertNotEmpty($response->errors);
+        $I->assertEquals($response->success, false);
+        $I->assertEquals($response->data, null);
+
+        $I->cantSeeInDatabase('monitoring_services', [
+            'id' => 555,
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => ValuesContainer::$projectWithEnvId,
+            'is_enabled' => 0,
+        ]);
+    }
+
+    public function updateServiceSuccessForAdmin(FunctionalTester $I, Scenario $scenario): void
+    {
+        $oAuth = new OAuthSteps($scenario);
+        $oAuth->login(ValuesContainer::$userAdmin['email'], ValuesContainer::$userAdmin['password']);
+
+        $I->sendPUT(ApiEndpoints::PROJECT . '/' . ValuesContainer::$projectWithEnvId . '/monitoring-services/1',
+            json_encode([
+                'url' => 'http://google.com',
+                'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+                'is_enabled' => 0,
+            ]));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $response = json_decode($I->grabResponse(), false);
+        $I->assertEmpty($response->errors);
+        $I->assertEquals($response->success, true);
+        $I->assertEquals($response->data, null);
+
+        $I->canSeeInDatabase('monitoring_services', [
+            'url' => 'http://google.com',
+            'notification_emails' => 'test@gmail.com, qwerty@gmail.com',
+            'project_id' => ValuesContainer::$projectWithEnvId,
+            'is_enabled' => 0,
+        ]);
     }
 }
